aa43a96d97fa9fd164102ca9db258d07e6a2c5ac
Sean Barag <sean@barag.org>
2017-12-20 17:09:29 -0800

    Restructure things a bit more & make a barebones CLI
    `commander` has significantly fewer dependencies than `yargs` and
    should still work reasonably well for this, so we'll use that.

---
+++ bin/cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const program = require("commander");
+const fs = require("fs");
+const path = require("path");
+
+// read current version from package.json
+// I'll _definitely_ forget to do this one day
+const packageJson = JSON.parse(
+    fs.readFileSync(
+        path.join(__dirname, "..", "package.json")
+    )
+);
+
+let brsFile;
+
+program
+    .description("Off-Roku BrightScript interpreter")
+    .arguments("orbs <filename>")
+    .action((filename) => brsFile = filename)
+    .version(packageJson.version);
+
+let args = program.parse(process.argv);

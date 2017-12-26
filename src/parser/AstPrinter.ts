51fb24cd698a20bc9870ccde47ad00c2624c1353
Sean Barag <sean@barag.org>
2017-12-26 12:08:21 -0500

    (parse) Add AST-printing visitor
    It's mostly used for debugging and doesn't support all token types,
    but it's a start!

---
+++ src/parser/AstPrinter.ts
@@ -0,0 +1,71 @@
+import * as Expr from "./Expression";
+import { Visitor } from "./Visitor";
+import { Literal as TokenLiteral, Invalid } from "../Token";
+
+/** Creates a pretty-printed representation of an expression to ease debugging. */
+export class AstPrinter implements Visitor<string> {
+    private indent: number;
+    /**
+     * Pretty-prints an expression for debugging purposes.
+     * @param expression the expression to pretty-print.
+     */
+    print(expression: Expr.Expression): string {
+        this.indent = 0;
+        return expression.accept(this);
+    }
+
+    visitAssign(e: Expr.Assign): string {
+        return JSON.stringify(e, undefined, 2);
+    }
+
+    visitBinary(e: Expr.Binary): string {
+        return this.parenthesize(e.token.text, e.left, e.right);
+    }
+    visitCall(e: Expr.Call): string {
+        return JSON.stringify(e, undefined, 2);
+    }
+    visitGet(e: Expr.Get): string {
+        return JSON.stringify(e, undefined, 2);
+    }
+    visitGrouping(e: Expr.Grouping): string {
+        return this.parenthesize("group", e.expression);
+    }
+    visitLiteral(e: Expr.Literal): string {
+        if (e.value == null) { return "invalid" }
+        else { return e.value.toString() };
+    }
+    visitLogical(e: Expr.Logical): string {
+        return JSON.stringify(e, undefined, 2);
+    }
+    visitM(e: Expr.M): string {
+        return JSON.stringify(e, undefined, 2);
+    }
+    visitSet(e: Expr.Set): string {
+        return JSON.stringify(e, undefined, 2);
+    }
+    visitUnary(e: Expr.Unary): string {
+        return this.parenthesize(e.operator.text, e.right);
+    }
+    visitVariable(expression: Expr.Variable): string {
+        return JSON.stringify(expression, undefined, 2);
+    }
+
+    /**
+     * Wraps an expression in parentheses to make its grouping visible during debugging.
+     * 
+     * @param name The name of the expression type being printed.
+     * @param expressions any subexpressions that need to be stringified as well.
+     */
+    private parenthesize(name: string = "", ...expressions: Expr.Expression[]): string {
+        this.indent++;
+        let out = [
+            `(${name}\n`,
+            expressions.map(e => {
+                return `${"  ".repeat(this.indent)}${e.accept(this)}\n`
+            }).join(""),
+            `${"  ".repeat(this.indent - 1)})`
+        ].join("");
+        this.indent--;
+        return out;
+    }
+}
--- src/parser/Visitor.ts
+++ src/parser/Visitor.ts
@@ -1,15 +1,15 @@
 import * as Expr from "./Expression";
 
-export interface Visitor<ExpressionType extends Expr.Expression> {
-    visitAssign(expression: Expr.Assign): ExpressionType;
-    visitBinary(expression: Expr.Binary): ExpressionType;
-    visitCall(expression: Expr.Call): ExpressionType;
-    visitGet(expression: Expr.Get): ExpressionType;
-    visitGrouping(expression: Expr.Grouping): ExpressionType;
-    visitLiteral(expression: Expr.Literal): ExpressionType;
-    visitLogical(expression: Expr.Logical): ExpressionType;
-    visitM(expression: Expr.M): ExpressionType;
-    visitSet(expression: Expr.Set): ExpressionType;
-    visitUnary(expression: Expr.Unary): ExpressionType;
-    visitVariable(expression: Expr.Variable): ExpressionType;
+export interface Visitor<T> {
+    visitAssign(expression: Expr.Assign): T;
+    visitBinary(expression: Expr.Binary): T;
+    visitCall(expression: Expr.Call): T;
+    visitGet(expression: Expr.Get): T;
+    visitGrouping(expression: Expr.Grouping): T;
+    visitLiteral(expression: Expr.Literal): T;
+    visitLogical(expression: Expr.Logical): T;
+    visitM(expression: Expr.M): T;
+    visitSet(expression: Expr.Set): T;
+    visitUnary(expression: Expr.Unary): T;
+    visitVariable(expression: Expr.Variable): T;
 }

bb8bfbf158291c7a7b5ef14c91deb6422f4b80c5
Sean Barag <sean@barag.org>
2017-12-30 10:33:42 -0500

    (parse) Start testing parser
    I'm not really sure if I like this pattern of building up an AST at
    runtime and comparing it to an executed one.  It's just really verbose
    and a pain to maintain.  Maybe something like Jest's
    [snapshots](http://facebook.github.io/jest/docs/en/snapshot-testing.html)
    would work here?  It matches what the TypeScript compiler does with
    benchmarks.  There's a few mocha equivalents around too, so maybe those
    would be better.

---
+++ test/Parser.js
@@ -0,0 +1,81 @@
+const { assert } = require("chai");
+
+const Parser = require("../lib/parser");
+const Expr = require("../lib/parser/Expression");
+const { Lexeme } = require("../lib/Lexeme");
+
+function token(kind, literal) {
+    return {
+        kind: kind,
+        literal: literal,
+        line: 1
+    };
+}
+
+const EOF = token(Lexeme.Eof);
+
+describe("parser", function() {
+    context("primary expressions", function() {
+        it("parses literals", function() {
+            let numeric = Parser.parse([
+                token(Lexeme.Integer, 5),
+                EOF
+            ]);
+
+            assert.deepEqual(
+                numeric,
+                new Expr.Literal(5),
+                "Must parse numeric literal tokens to literal AST nodes"
+            );
+
+            let parsedString = Parser.parse([
+                token(Lexeme.String, "hello"),
+                EOF
+            ]);
+            assert.deepEqual(
+                parsedString,
+                new Expr.Literal("hello"),
+                "Must parse string literal tokens to literal AST nodes"
+            );
+        });
+
+        it("parses identifiers");
+
+        it("parses expressions in parentheses", function() {
+            let withParens = Parser.parse([
+                token(Lexeme.Integer, 1),
+                token(Lexeme.Plus),
+                token(Lexeme.LeftParen),
+                token(Lexeme.Integer, 2),
+                token(Lexeme.Star),
+                token(Lexeme.Integer, 3),
+                token(Lexeme.RightParen),
+                EOF
+            ]);
+            let expectedAst = new Expr.Binary(
+                new Expr.Literal(1),
+                token(Lexeme.Plus),
+                new Expr.Grouping(
+                    new Expr.Binary(
+                        new Expr.Literal(2),
+                        token(Lexeme.Star),
+                        new Expr.Literal(3)
+                    )
+                )
+            );
+            assert.deepEqual(
+                withParens,
+                expectedAst,
+                "Must parse paranthetic tokens into paranthetic AST nodes"
+            );
+        });
+    }); // primary expressions
+
+    context("exponentiation", function() {
+        it("parses exponentiation operators");
+    }); // exponentiation
+
+    context("unary expressions", function() {
+        it("parses unary operators");
+    }); // unary expressions
+}); // parser

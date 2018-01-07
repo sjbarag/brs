3ac282afc1d6662080232da731cf0098addeb553
Sean Barag <sean@barag.org>
2018-01-06 19:59:59 -0500

    (parse) Add test for print statements

--- test/parser/statement/PrintStatements.test.js
+++ test/parser/statement/PrintStatements.test.js
@@ -8,6 +8,16 @@ describe("parser", () => {
     afterEach(() => BrsError.reset());
 
     describe("print statements", () => {
-        it("does some stuff");
+        it("parses print statements", () => {
+            let parsed = Parser.parse([
+                token(Lexeme.Print),
+                token(Lexeme.String, "Hello, world"),
+                EOF
+            ]);
+
+            expect(parsed).toBeDefined();
+            expect(parsed).not.toBeNull();
+            expect(parsed).toMatchSnapshot();
+        });
     });
 });

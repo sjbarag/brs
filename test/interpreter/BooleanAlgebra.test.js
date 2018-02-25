d79c1f0f46ca8672f10688c9894d72e164f7e69d
Sean Barag <sean@barag.org>
2018-02-24 17:30:36 -0800

    feat(lex,parse,interp): Fix tests caused by statement return refactor

--- test/interpreter/BooleanAlgebra.test.js
+++ test/interpreter/BooleanAlgebra.test.js
@@ -24,7 +24,7 @@ describe.only("interpreter boolean algebra", () => {
         );
 
         let [result] = interpreter.exec([ast]);
-        expect(result).toEqual(BrsTypes.BrsBoolean.False);
+        expect(result.value).toEqual(BrsTypes.BrsBoolean.False);
     });
 
     it("ORs booleans", () => {
@@ -37,7 +37,7 @@ describe.only("interpreter boolean algebra", () => {
         );
 
         let [result] = interpreter.exec([ast]);
-        expect(result).toEqual(BrsTypes.BrsBoolean.True);
+        expect(result.value).toEqual(BrsTypes.BrsBoolean.True);
     });
 
     it("doesn't allow mixed-type ANDs", () => {
@@ -51,7 +51,7 @@ describe.only("interpreter boolean algebra", () => {
 
         let [result] = interpreter.exec([ast]);
         expect(BrsError.found()).toBe(true);
-        expect(result).toEqual(BrsTypes.BrsInvalid.Instance);
+        expect(result.value).toEqual(BrsTypes.BrsInvalid.Instance);
     });
 
     it("doesn't allow mixed-type ORs", () => {
@@ -65,6 +65,6 @@ describe.only("interpreter boolean algebra", () => {
 
         let [result] = interpreter.exec([ast]);
         expect(BrsError.found()).toBe(true);
-        expect(result).toEqual(BrsTypes.BrsInvalid.Instance);
+        expect(result.value).toEqual(BrsTypes.BrsInvalid.Instance);
     });
 });

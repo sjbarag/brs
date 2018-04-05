edf75a7489052cc03d6754a8d11ec02f2d06bba1
Josh Stratton <strattonbrazil@gmail.com>
2018-04-04 20:52:18 -0700

    implemented Asc() and Chr() global string functions

--- test/stdlib/String.test.js
+++ test/stdlib/String.test.js
@@ -1,6 +1,6 @@
-const { UCase, LCase } = require("../../lib/stdlib/index");
+const { UCase, LCase, Asc, Chr } = require("../../lib/stdlib/index");
 const { Interpreter } = require("../../lib/interpreter");
-const { BrsString, BrsBoolean } = require("../../lib/brsTypes");
+const { BrsString, BrsBoolean, Int32 } = require("../../lib/brsTypes");
 
 const interpreter = new Interpreter();
 
@@ -22,4 +22,31 @@ describe("global string functions", () => {
             ).toEqual(new BrsString("l0rem"));
         });
     });
+
+    describe("Asc", () => {
+        it("converts an empty BRS string to zero", () => {
+            expect(
+                Asc.call(interpreter, new BrsString(""))
+            ).toEqual(new Int32(0));
+        })
+        it("converts a character to an ascii representation", () => {
+            expect(
+                Asc.call(interpreter, new BrsString("for"))
+            ).toEqual(new Int32(102)); // ascii decimal for "f"
+        })
+    })
+
+    describe("Chr", () => {
+        it("converts a non-integer to an empty string", () => {
+            expect(
+                Chr.call(interpreter, new BrsString("some string"))
+            ).toEqual(new BrsString(""));
+        })
+
+        it("converts an ascii integer to character", () => {
+            expect(
+                Chr.call(interpreter, new Int32(34))
+            ).toEqual(new BrsString("\""));
+        })
+    })
 })

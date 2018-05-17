30be3e7bea320ca98bdff36e0cbdaa528732bfa6
Sean Barag <sean@barag.org>
2018-05-16 18:10:02 -0700

    Add parser tests for print lists

--- test/parser/statement/PrintStatements.test.js
+++ test/parser/statement/PrintStatements.test.js
@@ -8,10 +8,40 @@ describe("parser", () => {
     afterEach(() => BrsError.reset());
 
     describe("print statements", () => {
-        it("parses print statements", () => {
+        it("parses singular print statements", () => {
             let parsed = Parser.parse([
                 token(Lexeme.Print),
                 token(Lexeme.String, "Hello, world"),
+                EOF
+            ]);
+
+            expect(parsed).toBeDefined();
+            expect(parsed).not.toBeNull();
+            expect(parsed).toMatchSnapshot();
+        });
+
+        it("parses print lists with no separator", () => {
+            let parsed = Parser.parse([
+                token(Lexeme.Print),
+                token(Lexeme.String, "Foo"),
+                token(Lexeme.String, "bar"),
+                token(Lexeme.String, "baz"),
+                EOF
+            ]);
+
+            expect(parsed).toBeDefined();
+            expect(parsed).not.toBeNull();
+            expect(parsed).toMatchSnapshot();
+        });
+
+        it("parses print lists with separators", () => {
+            let parsed = Parser.parse([
+                token(Lexeme.Print),
+                token(Lexeme.String, "Foo"),
+                token(Lexeme.Semicolon),
+                token(Lexeme.String, "bar"),
+                token(Lexeme.Comma),
+                token(Lexeme.String, "baz"),
                 EOF
             ]);
 

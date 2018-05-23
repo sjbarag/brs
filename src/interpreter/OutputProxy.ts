0b604c7542fdda0a8e1a6a38fadaa1dd44aa6121
Sean Barag <sean@barag.org>
2018-05-23 09:28:02 -0700

    Add docs for OutputProxy

--- src/interpreter/OutputProxy.ts
+++ src/interpreter/OutputProxy.ts
@@ -1,8 +1,17 @@
+/** Proxies a `stdout`-like stream to provide current-column tracking. */
 export class OutputProxy {
     currentLineLength = 0;
 
+    /** 
+     * Creates a new proxy that tracks the current column of the provided stream.
+     * @param outputStream the stream to proxy writes to
+     */
     constructor(private outputStream: NodeJS.WriteStream) { }
 
+    /**
+     * Writes a string's worth of data to the proxied stream and updates the current output column.
+     * @param str the string to write to the proxied stream
+     */
     write(str: string) {
         this.outputStream.write(str);
 
@@ -18,6 +27,14 @@ export class OutputProxy {
         this.currentLineLength += str.length; 
     }
 
+    /**
+     * Calculates and returns the column that the next written character will
+     * be placed in. If the proxied stream is a TTY, the current position will
+     * be in the range `[0, proxiedStream.columns)`.
+     * 
+     * @returns the zero-indexed position at which the next written character
+     *          will be placed in the proxied output stream.
+     */
     position() {
         if (!this.outputStream.isTTY || !this.outputStream.columns) {
             return this.currentLineLength;

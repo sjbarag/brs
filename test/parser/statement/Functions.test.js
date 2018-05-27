3d2149a3b77de4c6f10831652f386409332ee03c
Sean Barag <sean@barag.org>
2018-05-26 17:28:09 -0700

    test(parse): Test function parsing of function bodies

--- test/parser/statement/Functions.test.js
+++ test/parser/statement/Functions.test.js
@@ -1,6 +1,6 @@
 const Parser = require("../../../lib/parser");
 const { Lexeme } = require("../../../lib/Lexeme");
-const { Int32 } = require("../../../lib/brsTypes");
+const { BrsString, Int32 } = require("../../../lib/brsTypes");
 const BrsError = require("../../../lib/Error");
 
 const { EOF } = require("../ParserTests");
@@ -17,6 +17,26 @@ describe("parser", () => {
                 { kind: Lexeme.RightParen, text: ")", line: 1 },
                 { kind: Lexeme.Newline, text: "\\n", line: 1 },
                 { kind: Lexeme.EndFunction, text: "end function", line: 2 },
+                EOF
+            ]);
+
+            expect(BrsError.found()).toBeFalsy();
+            expect(parsed).toBeDefined();
+            expect(parsed).not.toBeNull();
+            expect(parsed).toMatchSnapshot();
+        });
+
+        it("parses non-empty function declarations", () => {
+            let parsed = Parser.parse([
+                { kind: Lexeme.Function, text: "function", line: 1 },
+                { kind: Lexeme.Identifier, text: "foo", line: 1 },
+                { kind: Lexeme.LeftParen, text: "(", line: 1 },
+                { kind: Lexeme.RightParen, text: ")", line: 1 },
+                { kind: Lexeme.Newline, text: "\\n", line: 1 },
+                { kind: Lexeme.Print, text: "print", line: 2 },
+                { kind: Lexeme.String, text: "Lorem ipsum", line: 2, literal: new BrsString("Lorem ipsum") },
+                { kind: Lexeme.Newline, text: "\\n", line: 2 },
+                { kind: Lexeme.EndFunction, text: "end function", line: 3 },
                 EOF
             ]);
 
@@ -114,6 +134,26 @@ describe("parser", () => {
                 { kind: Lexeme.RightParen, text: ")", line: 1 },
                 { kind: Lexeme.Newline, text: "\\n", line: 1 },
                 { kind: Lexeme.EndSub, text: "end sub", line: 2 },
+                EOF
+            ]);
+
+            expect(BrsError.found()).toBeFalsy();
+            expect(parsed).toBeDefined();
+            expect(parsed).not.toBeNull();
+            expect(parsed).toMatchSnapshot();
+        });
+
+        it("parses non-empty sub declarations", () => {
+            let parsed = Parser.parse([
+                { kind: Lexeme.Sub, text: "sub", line: 1 },
+                { kind: Lexeme.Identifier, text: "foo", line: 1 },
+                { kind: Lexeme.LeftParen, text: "(", line: 1 },
+                { kind: Lexeme.RightParen, text: ")", line: 1 },
+                { kind: Lexeme.Newline, text: "\\n", line: 1 },
+                { kind: Lexeme.Print, text: "print", line: 2 },
+                { kind: Lexeme.String, text: "Lorem ipsum", line: 2, literal: new BrsString("Lorem ipsum") },
+                { kind: Lexeme.Newline, text: "\\n", line: 2 },
+                { kind: Lexeme.EndSub, text: "end sub", line: 3 },
                 EOF
             ]);
 

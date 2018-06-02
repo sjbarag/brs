18bddba7525707e793cd021d66170eebf2c3f1db
Sean Barag <sean@barag.org>
2018-06-02 13:25:20 -0700

    fix: Add BrsFunction to git
    whoops, I forgot it

---
+++ src/interpreter/BrsFunction.ts
@@ -0,0 +1,33 @@
+import { Callable, BrsInvalid, BrsType } from "../brsTypes";
+import * as Stmt from "../parser/Statement";
+import { Interpreter } from "../interpreter";
+import { Expression } from "../parser/Expression";
+import Environment from "../interpreter/Environment";
+
+export function toCallable(declaration: Stmt.Function) {
+    return new Callable(
+        {
+            name: declaration.name.text,
+            args: declaration.parameters
+        },
+        (interpreter: Interpreter, ...args: BrsType[]) => {
+            // first, we need to evaluate all of the parameter default values
+            // and define them in a new environment
+            let environment = Environment.from(interpreter.environment);
+            declaration.parameters.forEach((param, index) => {
+                if (param.defaultValue) {
+                    environment.define(param.name, interpreter.evaluate(param.defaultValue));
+                    return;
+                }
+
+                environment.define(param.name, args[index])
+            });
+
+            let result = interpreter.execWith(declaration.body.statements, environment);
+
+            // then return whatever BrightScript returned
+            // TODO: Add support for return statements
+            return BrsInvalid.Instance;
+        }
+    );
+}

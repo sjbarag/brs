fb1ec3ce26e143963967650d60f8de772361a794
Sean Barag <sean@barag.org>
2018-06-18 09:37:39 -0700

    test(interp): Keep testing runtime function declarations

--- test/interpreter/Function.test.js
+++ test/interpreter/Function.test.js
@@ -5,7 +5,7 @@ const { identifier, token } = require("../parser/ParserTests");
 const { binary } = require("./InterpreterTests");
 const { Lexeme } = require("../../lib/Lexeme");
 const { Interpreter } = require("../../lib/interpreter");
-const { Int32, BrsString, BrsBoolean, BrsInvalid, Callable } = require("../../lib/brsTypes");
+const { Int32, BrsString, BrsBoolean, BrsInvalid, Callable, ValueKind } = require("../../lib/brsTypes");
 
 let interpreter;
 
@@ -26,11 +26,122 @@ describe("interpreter function declarations", () => {
         ];
 
         interpreter.exec(statements);
+        expect(BrsError.found()).toBe(false);
 
         let storedValue = interpreter.environment.get(
             { kind: Lexeme.Identifier, text: "foo", line: 3 }
         );
         expect(storedValue).not.toBe(BrsInvalid.Instance);
         expect(storedValue).toBeInstanceOf(Callable);
+    });
+
+    it("can call functions after definition", () => {
+        let emptyBlock = new Stmt.Block([]);
+        jest.spyOn(emptyBlock, "accept");
+
+        let statements = [
+            new Stmt.Function(
+                { kind: Lexeme.Identifier, text: "foo", line: 1 },
+                [],
+                emptyBlock
+            ),
+            new Stmt.Expression(
+                new Expr.Call(
+                    new Expr.Variable(
+                        { kind: Lexeme.Identifier, text: "foo", line: 3 }
+                    ),
+                    { kind: Lexeme.RightParen, text: ")", line: 3 },
+                    []
+                )
+            )
+        ];
+
+        interpreter.exec(statements);
+        expect(BrsError.found()).toBe(false);
+
+        expect(emptyBlock.accept).toHaveBeenCalledTimes(1);
+    });
+
+    it("returns values", () => {
+        let statements = [
+            new Stmt.Function(
+                { kind: Lexeme.Identifier, text: "foo", line: 1 },
+                [],
+                new Stmt.Block([
+                    new Stmt.Return(
+                        { kind: Lexeme.Return, text: "return", line: 2 },
+                        new Expr.Literal(
+
+                            new BrsString("hello, world")
+                        )
+                    )
+                ])
+            ),
+            new Stmt.Assignment(
+                { kind: Lexeme.Identifier, text: "result", line: 4},
+                new Expr.Call(
+                    new Expr.Variable(
+                        { kind: Lexeme.Identifier, text: "foo", line: 4 }
+                    ),
+                    { kind: Lexeme.RightParen, text: ")", line: 4 },
+                    []
+                )
+            )
+        ];
+
+        interpreter.exec(statements);
+        expect(BrsError.found()).toBe(false);
+
+        let storedResult = interpreter.environment.get(
+            { kind: Lexeme.Identifier, text: "result", line: 5 }
+        );
+        expect(storedResult).toEqual(new BrsString("hello, world"));
+    });
+
+    it("evaluates functions into their own scope", () => {
+        throw "TODO";
+    });
+
+    it("evaluates default arguments", () => {
+        let statements = [
+            new Stmt.Function(
+                { kind: Lexeme.Identifier, text: "ident", line: 1 },
+                [{
+                    name: "input",
+                    type: ValueKind.String,
+                    defaultValue: new Expr.Literal(new Int32(-32))
+                }],
+                new Stmt.Block([
+                    new Stmt.Return(
+                        { kind: Lexeme.Return, text: "return", line: 2 },
+                        new Expr.Variable(
+                            { kind: Lexeme.Identifier, text: "input", line: 2 }
+                        )
+                    )
+                ])
+            ),
+            new Stmt.Assignment(
+                { kind: Lexeme.Identifier, text: "result", line: 4 },
+                new Expr.Call(
+                    new Expr.Variable(
+                        { kind: Lexeme.Identifier, text: "ident", line: 4 }
+                    ),
+                    { kind: Lexeme.RightParen, text: ")", line: 4 },
+                    []
+                )
+            )
+        ];
+
+        interpreter.exec(statements);
+        expect(BrsError.found()).toBe(false);
+
+        let storedResult = interpreter.environment.get(
+            { kind: Lexeme.Identifier, text: "result", line: 5 }
+        );
+        expect(storedResult).toEqual(new Int32(-32));
+
+        expect(interpreter.environment.has(
+            { kind: Lexeme.Identifier, text: "input", line: 6 }
+        )).toBe(false);
     });
 });

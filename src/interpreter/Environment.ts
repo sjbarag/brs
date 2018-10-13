d2bc299ccd935b0c45df6bf81d9220e2d720de82
Sean Barag <sean@barag.org>
2018-10-13 08:26:44 -0700

    fix(scope): Remove function-scoped variables when entering new function
    RBI doesn't create closures when functions are created.  To ensure that
    only function parameters, functions compiled into this module, and
    global functions are accessible, we need to remove any variables that
    exist in the current scope before executing a function.

--- src/interpreter/Environment.ts
+++ src/interpreter/Environment.ts
@@ -3,32 +3,102 @@ import { Lexeme } from "../Lexeme";
 import { BrsType } from "../brsTypes";
 import * as BrsError from "../Error";
 
-export default class Environment {
-    private values = new Map<string, BrsType>();
+/** The logical region from which a particular variable or function that defines where it may be accessed from. */
+export enum Scope {
+    /** The set of native functions that are always accessible, e.g. `RebootSystem`. */
+    Global,
+    /** The set of named functions accessible from a set of files compiled together. */
+    Module,
+    /** The set of variables (including anonymous functions) accessible *only* from within a function body. */
+    Function
+}
 
-    public define(name: string, value: BrsType): void {
-        this.values.set(name.toLowerCase(), value);
+/** An error thrown when attempting to access an uninitialized variable. */
+export class NotFound extends Error {
+    constructor(reason: string) {
+        super(reason);
+    }
+}
+
+/**
+ * Holds a set of values in multiple scopes and provides access operations to them.
+ */
+export class Environment {
+    /**
+     * Functions that are always accessible.
+     * @see Scope.Global
+     */
+    private global = new Map<string, BrsType>();
+    /**
+     * Named functions that are compiled together into a single module.
+     * @see Scope.Module
+     */
+    private module = new Map<string, BrsType>();
+    /**
+     * Variables and anonymous functions accessible only within a function's body.
+     * @see Scope.Function
+     */
+    private function = new Map<string, BrsType>();
+
+    public define(scope: Scope, name: string, value: BrsType): void {
+        let destination: Map<string, BrsType>;
+
+        switch (scope) {
+            case Scope.Function:
+                destination = this.function;
+                break;
+            case Scope.Module:
+                destination = this.module;
+                break;
+            default:
+                destination = this.global;
+                break;
+        }
+
+        destination.set(name.toLowerCase(), value);
     }
 
     public remove(name: string): void {
-        this.values.delete(name.toLowerCase());
+        this.function.delete(name.toLowerCase());
     }
 
     public get(name: Token): BrsType {
-        if (this.values.has(name.text!.toLowerCase())) {
-            return this.values.get(name.text!.toLowerCase())!;
+        let lowercaseName = name.text!.toLowerCase();
+        let source = [this.function, this.module, this.global].find(scope =>
+            scope.has(lowercaseName)
+        );
+
+        if (source) {
+            return source.get(lowercaseName)!;
         }
 
-        throw BrsError.runtime(`Undefined variable '${name.text}'`, name.line);
+        throw new NotFound(`Undefined variable '${name.text}'`);
     }
 
     public has(name: Token): boolean {
-        return this.values.has((name.text || "").toLowerCase());
+        let lowercaseName = name.text!.toLowerCase();
+        return [this.function, this.module, this.global].find(scope => scope.has(lowercaseName)) != null;
     }
 
+    /**
+     * Creates a clone of an environment, but without its function-scoped
+     * values. Useful for creating sub-environments.
+     *
+     * The Reference BrightScript Implementation (RBI) doesn't currently create closures when
+     * functions are created.  When a function is called, it has access only to:
+     *
+     * 1. Globally-defined functions (e.g. `RebootSystem`, `UCase`, et. al.)
+     * 2. Named functions compiled together into a single "module"
+     * 3. Parameters passed into the function
+     *
+     * @param original the source environment
+     * @returns a copy of `original` but with no function-scoped values.
+     */
     static from(original: Environment) {
         let newEnvironment = new Environment();
-        newEnvironment.values = new Map<string, BrsType>(original.values);
+        newEnvironment.global = original.global;
+        newEnvironment.module = original.module;
+
         return newEnvironment;
     }
 }

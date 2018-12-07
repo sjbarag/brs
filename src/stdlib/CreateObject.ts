d7df5313e24d5f567410ab0ffcdaf4b011f7ecfd
Sean Barag <sean.barag@hulu.com>
2018-12-07 13:04:42 -0800

    test(roArray): Add end-to-end tests for roArray

--- src/stdlib/CreateObject.ts
+++ src/stdlib/CreateObject.ts
@@ -1,5 +1,6 @@
-import { Callable, ValueKind, BrsInvalid, BrsString } from "../brsTypes";
+import { Callable, ValueKind, BrsInvalid, BrsString, BrsType } from "../brsTypes";
 import { BrsObjects } from "../brsTypes/components/BrsObjects";
+import * as Expr from "../parser/Expression";
 import { Interpreter } from "../interpreter";
 
 /** Creates a new instance of a given brightscript component (e.g. roAssociativeArray) */
@@ -7,12 +8,19 @@ export const CreateObject = new Callable(
     "CreateObject",
     {
         signature: {
-            args: [{ name: "objName", type: ValueKind.String }],
+            args: [
+                { name: "objName", type: ValueKind.String },
+                { name: "arg1", type: ValueKind.Dynamic, defaultValue: new Expr.Literal(BrsInvalid.Instance) },
+                { name: "arg2", type: ValueKind.Dynamic, defaultValue: new Expr.Literal(BrsInvalid.Instance) },
+                { name: "arg3", type: ValueKind.Dynamic, defaultValue: new Expr.Literal(BrsInvalid.Instance) },
+                { name: "arg4", type: ValueKind.Dynamic, defaultValue: new Expr.Literal(BrsInvalid.Instance) },
+                { name: "arg4", type: ValueKind.Dynamic, defaultValue: new Expr.Literal(BrsInvalid.Instance) }
+            ],
             returns: ValueKind.Dynamic
         },
-        impl: (interpreter: Interpreter, objName: BrsString) => {
+        impl: (interpreter: Interpreter, objName: BrsString, ...additionalArgs: BrsType[]) => {
             let ctor = BrsObjects.get(objName.value.toLowerCase());
-            return ctor ? ctor() : BrsInvalid.Instance;
+            return ctor ? ctor(...additionalArgs) : BrsInvalid.Instance;
         }
     }
 );

f51098d652dfa4579797b7215f8a4b7f7908d084
Sean Barag <sean.barag@hulu.com>
2018-12-07 12:34:04 -0800

    feat(ifArray): Implement ifArray on BrsArray type

--- src/brsTypes/components/BrsArray.ts
+++ src/brsTypes/components/BrsArray.ts
@@ -3,6 +3,7 @@ import { BrsType } from "..";
 import { BrsComponent, BrsIterable } from "./BrsComponent";
 import { Callable } from "../Callable";
 import { Interpreter } from "../../interpreter";
+import { Int32 } from "../Int32";
 
 export class BrsArray extends BrsComponent implements BrsValue, BrsIterable {
     readonly kind = ValueKind.Object;
@@ -15,8 +16,12 @@ export class BrsArray extends BrsComponent implements BrsValue, BrsIterable {
             this.peek,
             this.pop,
             this.push,
+            this.shift,
+            this.unshift,
+            this.delete,
+            this.count,
             this.clear,
-            this.shift
+            this.append
         ]);
     }
 
@@ -74,16 +79,28 @@ export class BrsArray extends BrsComponent implements BrsValue, BrsIterable {
         return BrsInvalid.Instance;
     }
 
-    private clear = new Callable(
-        "clear",
+    private peek = new Callable(
+        "peek",
         {
             signature: {
                 args: [],
-                returns: ValueKind.Void
+                returns: ValueKind.Dynamic
             },
             impl: (interpreter: Interpreter) => {
-                this.elements = [];
-                return BrsInvalid.Instance;
+                return this.elements[this.elements.length - 1] || BrsInvalid.Instance;
+            }
+        }
+    );
+
+    private pop = new Callable(
+        "pop",
+        {
+            signature: {
+                args: [],
+                returns: ValueKind.Dynamic
+            },
+            impl: (interpreter: Interpreter) => {
+                return this.elements.pop() || BrsInvalid.Instance;
             }
         }
     );
@@ -104,32 +121,6 @@ export class BrsArray extends BrsComponent implements BrsValue, BrsIterable {
         }
     );
 
-    private pop = new Callable(
-        "pop",
-        {
-            signature: {
-                args: [],
-                returns: ValueKind.Dynamic
-            },
-            impl: (interpreter: Interpreter) => {
-                return this.elements.pop() || BrsInvalid.Instance;
-            }
-        }
-    );
-
-    private peek = new Callable(
-        "peek",
-        {
-            signature: {
-                args: [],
-                returns: ValueKind.Dynamic
-            },
-            impl: (interpreter: Interpreter) => {
-                return this.elements[this.elements.length - 1] || BrsInvalid.Instance;
-            }
-        }
-    );
-
     private shift = new Callable(
         "shift",
         {
@@ -143,5 +134,92 @@ export class BrsArray extends BrsComponent implements BrsValue, BrsIterable {
         }
     );
 
-    // TODO: implement append!
+    private unshift = new Callable(
+        "unshift",
+        {
+            signature: {
+                args: [
+                    { name: "tvalue", type: ValueKind.Dynamic }
+                ],
+                returns: ValueKind.Void
+            },
+            impl: (interpreter: Interpreter, tvalue: BrsType) => {
+                this.elements.unshift(tvalue);
+                return BrsInvalid.Instance;
+            }
+        }
+    );
+
+    private delete = new Callable(
+        "delete",
+        {
+            signature: {
+                args: [
+                    { name: "index", type: ValueKind.Int32 }
+                ],
+                returns: ValueKind.Boolean
+            },
+            impl: (interpreter: Interpreter, index: Int32) => {
+                if (index.lessThan(new Int32(0)).toBoolean()) {
+                    return BrsBoolean.False;
+                }
+
+                let deleted = this.elements.splice(index.getValue(), 1);
+                return BrsBoolean.from(deleted.length > 0);
+            }
+        }
+    );
+
+    private count = new Callable(
+        "count",
+        {
+            signature: {
+                args: [],
+                returns: ValueKind.Int32
+            },
+            impl: (interpreter: Interpreter) => {
+                return new Int32(this.elements.length);
+            }
+        }
+    );
+
+    private clear = new Callable(
+        "clear",
+        {
+            signature: {
+                args: [],
+                returns: ValueKind.Void
+            },
+            impl: (interpreter: Interpreter) => {
+                this.elements = [];
+                return BrsInvalid.Instance;
+            }
+        }
+    );
+
+    private append = new Callable(
+        "append",
+        {
+            signature: {
+                args: [{
+                    name: "array",
+                    type: ValueKind.Object
+                }],
+                returns: ValueKind.Void
+            },
+            impl: (interpreter: Interpreter, array: BrsComponent) => {
+                if (!(array instanceof BrsArray)) {
+                    // TODO: validate against RBI
+                    return BrsInvalid.Instance;
+                }
+
+                this.elements = [
+                    ...this.elements,
+                    ...array.elements.filter(element => !!element) // don't copy "holes" where no value exists
+                ];
+
+                return BrsInvalid.Instance;
+            }
+        }
+    );
 }

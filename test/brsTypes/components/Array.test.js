17e8dd186ae98ff1ce1715307604f25677680722
Sean Barag <sean.barag@hulu.com>
2018-12-07 12:34:04 -0800

    feat(ifArray): Implement ifArray on BrsArray type

--- test/brsTypes/components/Array.test.js
+++ test/brsTypes/components/Array.test.js
@@ -1,6 +1,7 @@
 const { BrsTypes } = require("brs");
 const { BrsArray, BrsBoolean, BrsString, Int32, BrsInvalid } = BrsTypes;
 const BrsError = require("../../../lib/Error");
+const { Interpreter } = require("../../../lib/interpreter");
 
 describe("Array", () => {
     beforeEach(() => BrsError.reset());
@@ -80,6 +81,196 @@ describe("Array", () => {
 
             arr.set(new Int32(555), new BrsString("value set at index 555"));
             expect(arr.get(new Int32(555))).toEqual(new BrsString("value set at index 555"));
+        });
+    });
+
+    describe("methods", () => {
+        let interpreter;
+
+        beforeEach(() => {
+            interpreter = new Interpreter();
+        });
+
+        describe("peek", () => {
+            it("returns the value at the highest index", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([a, b, c]);
+
+                let peek = arr.getMethod("peek");
+                expect(peek).toBeTruthy();
+                expect(peek.call(interpreter)).toBe(c);
+            });
+
+            it("returns `invalid` when empty", () => {
+                let arr = new BrsArray([]);
+
+                let peek = arr.getMethod("peek");
+                expect(peek).toBeTruthy();
+                expect(peek.call(interpreter)).toBe(BrsInvalid.Instance);
+            });
+        });
+
+        describe("pop", () => {
+            it("returns and removes the value at the highest index", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([a, b, c]);
+
+                let pop = arr.getMethod("pop");
+                expect(pop).toBeTruthy();
+                expect(pop.call(interpreter)).toBe(c);
+                expect(arr.elements).toEqual([ a, b ]);
+            });
+
+            it("returns `invalid` and doesn't modify when empty", () => {
+                let arr = new BrsArray([]);
+
+                let pop = arr.getMethod("pop");
+                expect(pop).toBeTruthy();
+
+                let before = arr.getElements();
+                expect(pop.call(interpreter)).toBe(BrsInvalid.Instance);
+                expect(arr.getElements()).toEqual(before);
+            });
+        });
+
+        describe("push", () => {
+            it("appends a value to the end of the array", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([a, b]);
+
+                let push = arr.getMethod("push");
+                expect(push).toBeTruthy();
+                expect(push.call(interpreter, c)).toBe(BrsInvalid.Instance);
+                expect(arr.elements).toEqual([ a, b, c ]);
+            });
+        });
+
+
+        describe("shift", () => {
+            it("returns and removes the value at the lowest index", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([a, b, c]);
+
+                let shift = arr.getMethod("shift");
+                expect(shift).toBeTruthy();
+                expect(shift.call(interpreter)).toBe(a);
+                expect(arr.elements).toEqual([ b, c ]);
+            });
+
+            it("returns `invalid` and doesn't modify when empty", () => {
+                let arr = new BrsArray([]);
+
+                let shift = arr.getMethod("shift");
+                expect(shift).toBeTruthy();
+
+                let before = arr.getElements();
+                expect(shift.call(interpreter)).toBe(BrsInvalid.Instance);
+                expect(arr.getElements()).toEqual(before);
+            });
+        });
+
+        describe("unshift", () => {
+            it("inserts a value at the beginning of the array", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([b, c]);
+
+                let unshift = arr.getMethod("unshift");
+                expect(unshift).toBeTruthy();
+                expect(unshift.call(interpreter, a)).toBe(BrsInvalid.Instance);
+                expect(arr.elements).toEqual([ a, b, c ]);
+            });
+        });
+
+        describe("delete", () => {
+            it("removes elements from in-bounds indices", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([a, b, c]);
+
+                let deleteMethod = arr.getMethod("delete");
+                expect(deleteMethod).toBeTruthy();
+                expect(deleteMethod.call(interpreter, new Int32(1))).toBe(BrsBoolean.True);
+                expect(arr.elements).toEqual([ a, c ]);
+            });
+
+            it("doesn't remove elements from out-of-bounds indices", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([a, b, c]);
+
+                let deleteMethod = arr.getMethod("delete");
+                expect(deleteMethod).toBeTruthy();
+                expect(deleteMethod.call(interpreter, new Int32(1111))).toBe(BrsBoolean.False);
+                expect(deleteMethod.call(interpreter, new Int32(-1))).toBe(BrsBoolean.False);
+                expect(arr.elements).toEqual([ a, b, c ]);
+            });
+        });
+
+        describe("count", () => {
+            it("returns the length of the array", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([a, b, c]);
+
+                let count = arr.getMethod("count");
+                expect(count).toBeTruthy();
+                expect(count.call(interpreter)).toEqual(new Int32(3));
+            });
+        });
+
+        describe("clear", () => {
+            it("empties the array", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let c = new BrsString("c");
+
+                let arr = new BrsArray([a, b]);
+
+                let clear = arr.getMethod("clear");
+                expect(clear).toBeTruthy();
+                expect(clear.call(interpreter)).toBe(BrsInvalid.Instance);
+                expect(arr.elements).toEqual([]);
+            });
+        });
+
+        describe("append", () => {
+            it("adds non-empty elements to the current array", () => {
+                let a = new BrsString("a");
+                let b = new BrsString("b");
+                let src = new BrsArray([a, b]);
+
+                let c = new BrsString("c");
+                let d = new BrsString("d");
+                let e = new BrsString("e");
+                let f = new BrsString("f");
+                let other = new BrsArray([c, undefined, d, undefined, undefined, e, f, undefined]);
+
+                let append = src.getMethod("append");
+                expect(append).toBeTruthy();
+                expect(append.call(interpreter, other)).toBe(BrsInvalid.Instance);
+                expect(src.elements).toEqual([a, b, c, d, e, f]);
+            });
         });
     });
 });

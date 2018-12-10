4e718a5d02e83669445f4e3ea398d0eef8d16bbc
John Fearnside <john.fearnside@hulu.com>
2018-12-10 15:18:06 -0800

    Add FormatJson, ParseJson with failing tests (RED)

---
+++ src/stdlib/Json.ts
@@ -0,0 +1,36 @@
+import { BrsInvalid, BrsString, BrsType, Callable, Int32, ValueKind } from "../brsTypes";
+import { Interpreter } from "../interpreter";
+import * as Expr from "../parser/Expression";
+
+export const FormatJson = new Callable("FormatJson", {
+    signature: { returns: ValueKind.String, args: [
+        { name: "json", type: ValueKind.Object },
+        { name: "flags", type: ValueKind.Int32, defaultValue: new Expr.Literal(new Int32(0)) }
+    ]},
+    impl: (interpreter: Interpreter, json: BrsType, flags: Int32) => {
+        try {
+            return new BrsString(`"TODO: FormatJson"`);
+            // TODO: JSON.stringify
+        } catch (err) {
+            // example RBI error format: "BRIGHTSCRIPT: ERROR: FormatJSON: Value type not supported: roFunction: pkg:/source/main.brs(14)"
+            console.error(`FormatJSON: ${err.message}`);
+            return new BrsString("");
+        }
+    }
+});
+
+export const ParseJson = new Callable("ParseJson", {
+    signature: { returns: ValueKind.Dynamic, args: [
+        { name: "jsonString", type: ValueKind.String }
+    ]},
+    impl: (interpreter: Interpreter, jsonString: BrsString) => {
+        try {
+            return new BrsString(`"TODO: ParseJson"`);
+            // TODO: JSON.parse
+        } catch (err) {
+            // example RBI error format: "BRIGHTSCRIPT: ERROR: ParseJSON: Unknown identifier 'I'm not JSON': pkg:/source/main.brs(25)"
+            console.error(`ParseJSON: ${err.message}`);
+            return BrsInvalid.Instance;
+        }
+    }
+});

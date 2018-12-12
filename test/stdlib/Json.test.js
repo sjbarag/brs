41a0c6cc2fada9323ca16b767aadbfae056f00c1
John Fearnside <john.fearnside@hulu.com>
2018-12-12 12:25:26 -0800

    Add failing bare false tests (RED)

--- test/stdlib/Json.test.js
+++ test/stdlib/Json.test.js
@@ -1,15 +1,21 @@
 const { Interpreter } = require('../../lib/interpreter');
-const { BrsInvalid, BrsString, Int32 } = require("../../lib/brsTypes");
+const { BrsBoolean, BrsInvalid, BrsString } = require("../../lib/brsTypes");
 const { FormatJson, ParseJson } = require("../../lib/stdlib/index");
 
 describe('global JSON functions', () => {
     let interpreter = new Interpreter();
     let bareNull = new BrsString('null')
+    let bareFalse = new BrsString('false')
 
     describe('FormatJson', () => {
         it('converts BRS invalid to bare null string', () => {
             actual = FormatJson.call(interpreter, BrsInvalid.Instance);
             expect(actual).toMatchObject(bareNull);
+        });
+
+        it('converts BRS false to bare false string', () => {
+            actual = FormatJson.call(interpreter, BrsBoolean.False);
+            expect(actual).toMatchObject(bareFalse);
         });
     });
 
@@ -17,6 +23,11 @@ describe('global JSON functions', () => {
         it('converts bare null string to BRS invalid', () => {
             actual = ParseJson.call(interpreter, bareNull);
             expect(actual).toBe(BrsInvalid.Instance);
+        });
+
+        it('converts bare false string to BRS false', () => {
+            actual = ParseJson.call(interpreter, bareFalse);
+            expect(actual).toBe(BrsBoolean.False);
         });
     });
 });

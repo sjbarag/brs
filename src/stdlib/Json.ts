da7d15c1850cc656d78d5978954aff2c53e6a00f
John Fearnside <john.fearnside@hulu.com>
2018-12-12 12:14:32 -0800

    Refactor (GREEN)

--- src/stdlib/Json.ts
+++ src/stdlib/Json.ts
@@ -2,15 +2,32 @@ import { BrsInvalid, BrsString, BrsType, Callable, Int32, ValueKind } from "../b
 import { Interpreter } from "../interpreter";
 import * as Expr from "../parser/Expression";
 
+function jsonValueOf(brsValue: any): any {
+    if (brsValue.hasOwnProperty("value")) { return brsValue.value; }
+    switch (brsValue.kind) {
+    case ValueKind.Invalid:
+        return null;
+    default:
+        throw `jsonValueOf not implemented for: ${brsValue}`;
+    }
+}
+
+function brsValueOf(jsonValue: any): any {
+    if (jsonValue === null) { return BrsInvalid.Instance; }
+    switch (typeof jsonValue) {
+    default:
+        throw `brsValueOf not implemented for: ${jsonValue}`;
+    }
+}
+
 export const FormatJson = new Callable("FormatJson", {
     signature: { returns: ValueKind.String, args: [
-        { name: "json", type: ValueKind.Object },
+        { name: "x", type: ValueKind.Object },
         { name: "flags", type: ValueKind.Int32, defaultValue: new Expr.Literal(new Int32(0)) }
     ]},
-    impl: (interpreter: Interpreter, json: BrsType, flags: Int32) => {
+    impl: (_: Interpreter, x: BrsType, flags: Int32) => {
         try {
-            return new BrsString('null');
-            // TODO: JSON.stringify
+            return new BrsString(JSON.stringify(jsonValueOf(x)));
         } catch (err) {
             // example RBI error format: "BRIGHTSCRIPT: ERROR: FormatJSON: Value type not supported: roFunction: pkg:/source/main.brs(14)"
             console.error(`FormatJSON: ${err.message}`);
@@ -23,10 +40,9 @@ export const ParseJson = new Callable("ParseJson", {
     signature: { returns: ValueKind.Dynamic, args: [
         { name: "jsonString", type: ValueKind.String }
     ]},
-    impl: (interpreter: Interpreter, jsonString: BrsString) => {
+    impl: (_: Interpreter, jsonString: BrsString) => {
         try {
-            return BrsInvalid.InstancePass;
-            // TODO: JSON.parse
+            return brsValueOf(JSON.parse(jsonString.value));
         } catch (err) {
             // example RBI error format: "BRIGHTSCRIPT: ERROR: ParseJSON: Unknown identifier 'I'm not JSON': pkg:/source/main.brs(25)"
             console.error(`ParseJSON: ${err.message}`);

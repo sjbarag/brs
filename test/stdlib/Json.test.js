ed8228d62877ea0bb302c7d267dd1642320d7a3e
John Fearnside <john.fearnside@hulu.com>
2018-12-12 12:14:32 -0800

    Refactor (GREEN)

--- test/stdlib/Json.test.js
+++ test/stdlib/Json.test.js
@@ -4,18 +4,19 @@ const { FormatJson, ParseJson } = require("../../lib/stdlib/index");
 
 describe('global JSON functions', () => {
     let interpreter = new Interpreter();
+    let bareNull = new BrsString('null')
 
     describe('FormatJson', () => {
-        it('converts invalid to bare null', () => {
+        it('converts BRS invalid to bare null string', () => {
             actual = FormatJson.call(interpreter, BrsInvalid.Instance);
-            expect(actual).toMatchObject(new BrsString('null'));
+            expect(actual).toMatchObject(bareNull);
         });
     });
 
     describe('ParseJson', () => {
-        it('converts bare null to invalid', () => {
-            actual = ParseJson.call(interpreter, new BrsString('null'));
-            expect(actual).toMatchObject(BrsInvalid.Instance);
+        it('converts bare null string to BRS invalid', () => {
+            actual = ParseJson.call(interpreter, bareNull);
+            expect(actual).toBe(BrsInvalid.Instance);
         });
     });
 });

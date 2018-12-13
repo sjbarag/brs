16f7795be92d0b3653a1d6fd730d88486453df84
John Fearnside <john.fearnside@hulu.com>
2018-12-13 15:55:56 -0800

    Add/skip integer, longInteger, float tests

--- test/stdlib/Json.test.js
+++ test/stdlib/Json.test.js
@@ -4,16 +4,40 @@ const {
     BrsBoolean,
     BrsInvalid,
     BrsString,
+    Double,
+    Float,
+    Int32,
+    Int64,
     Uninitialized
 } = require("../../lib/brsTypes");
 
 describe('global JSON functions', () => {
     let interpreter = new Interpreter();
-    let brsBareNull = new BrsString('null')
-    let brsBareFalse = new BrsString('false')
-    let brsEmpty = new BrsString('')
-    let brsUnquoted = new BrsString('ok')
-    let brsQuoted = new BrsString('"ok"')
+
+    let brsBareNull = new BrsString('null');
+
+    let brsBareFalse = new BrsString('false');
+
+    let brsEmpty = new BrsString('');
+
+    let brsUnquoted = new BrsString('ok');
+    let brsQuoted = new BrsString('"ok"');
+
+    let floatStr = '3.402823061e+38';
+    let brsFloat = Float.fromString(floatStr);
+    let brsBareFloat = new BrsString(floatStr);
+
+    let integerStr = '2147483647';
+    let brsInteger = Int32.fromString(integerStr);
+    let brsBareInteger = new BrsString(integerStr);
+
+    let longIntegerStr = '9223372036854775807';
+    let brsLongInteger = Int64.fromString(longIntegerStr);
+    let brsBareLongInteger = new BrsString(longIntegerStr);
+
+    let doubleStr = '1.7976931348623e+308';
+    let brsDouble = Double.fromString(doubleStr);
+    let brsBareDouble = new BrsString(doubleStr);
 
     describe('FormatJson', () => {
         it('rejects non-convertible types', () => {
@@ -37,6 +61,21 @@ describe('global JSON functions', () => {
         it('converts BRS string to bare (quoted) string', () => {
             actual = FormatJson.call(interpreter, brsUnquoted);
             expect(actual).toMatchObject(brsQuoted);
+        });
+
+        it('converts BRS integer to bare integer string', () => {
+            actual = FormatJson.call(interpreter, brsInteger);
+            expect(actual).toMatchObject(brsBareInteger);
+        });
+
+        xit('converts BRS longInteger to bare longInteger string', () => {
+            actual = FormatJson.call(interpreter, brsLongInteger);
+            expect(actual).toMatchObject(brsBareLongInteger);
+        });
+
+        xit('converts BRS float to bare float string', () => {
+            actual = FormatJson.call(interpreter, brsFloat);
+            expect(actual).toMatchObject(brsBareFloat);
         });
     });
 
@@ -62,6 +101,21 @@ describe('global JSON functions', () => {
         it('converts bare (quoted) string to BRS string', () => {
             actual = ParseJson.call(interpreter, brsQuoted);
             expect(actual).toMatchObject(brsUnquoted);
+        });
+
+        xit('converts bare integer string to BRS integer', () => {
+            actual = ParseJson.call(interpreter, brsBareInteger);
+            expect(actual).toMatchObject(brsInteger);
+        });
+
+        xit('converts bare longInteger string to BRS longInteger', () => {
+            actual = ParseJson.call(interpreter, brsBareLongInteger);
+            expect(actual).toMatchObject(brsLongInteger);
+        });
+
+        xit('converts bare float string to BRS float', () => {
+            actual = ParseJson.call(interpreter, brsBareFloat);
+            expect(actual).toMatchObject(brsFloat);
         });
     });
 });

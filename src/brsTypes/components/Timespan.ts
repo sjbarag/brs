831646b8d74f01b79191c7bf74153dde040a7583
Jessica <jessica.weeber@hulu.com>
2018-12-19 15:54:30 -0800

    Implement brs totalMilliseconds() function for roTimespan

--- src/brsTypes/components/Timespan.ts
+++ src/brsTypes/components/Timespan.ts
@@ -1,28 +1,29 @@
 import { BrsValue, ValueKind, BrsString, BrsBoolean, BrsInvalid } from "../BrsType";
 import { BrsComponent, BrsIterable } from "./BrsComponent";
-import { BrsType } from "..";
+import { BrsType, isBrsNumber } from "..";
 import { Callable } from "../Callable";
 import { Interpreter } from "../../interpreter";
 import { Int32 } from "../Int32";
+import { ftruncate } from "fs";
 
 export class Timespan extends BrsComponent implements BrsValue {
     readonly kind = ValueKind.Object;
-    private start = new Int32(Date.now());
+    private start = Date.now();
 
     constructor() {
         super("roTimespan");
         this.registerMethods([
             this.mark,
             this.totalmilliseconds,
-            this.totalseconds,
-            this.getsecondstoiso8601date
+            // this.totalseconds,
+            // this.getsecondstoiso8601date
         ]);
 
         this.resetTime();
     }
 
     resetTime() {
-        this.start = new Int32(Date.now());
+        this.start = Date.now();
         console.log(this.start);
     }
 
@@ -42,5 +43,18 @@ export class Timespan extends BrsComponent implements BrsValue {
                 return BrsInvalid.Instance;
             }
         }
-    ); 
+    );
+
+    private totalmilliseconds = new Callable(
+        "totalmilliseconds",
+        {
+            signature: {
+                args: [],
+                returns: ValueKind.Int32
+            },
+            impl: (_: Interpreter) => {
+               return new Int32(Date.now() - this.start);
+            }
+        }
+    );
 }

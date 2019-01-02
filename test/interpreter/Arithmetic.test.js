797dc5585ea008dccb0c6af88718f63ef31a7b75
Sean Barag <sean.barag@hulu.com>
2019-01-02 13:10:17 -0800

    fix(test): Make arithmetic.test.js async

--- test/interpreter/Arithmetic.test.js
+++ test/interpreter/Arithmetic.test.js
@@ -14,55 +14,55 @@ describe("interpreter arithmetic", () => {
         interpreter = new Interpreter();
     });
 
-    it("adds numbers", () => {
+    it("adds numbers", async () => {
         let ast = binary(new BrsTypes.Int32(2), Lexeme.Plus, new BrsTypes.Float(1.5));
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(3.5);
     });
 
-    it("concatenates strings", () => {
+    it("concatenates strings", async () => {
         let ast = binary(new BrsTypes.BrsString("judge "), Lexeme.Plus, new BrsTypes.BrsString("judy"));
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.toString()).toBe("judge judy");
     });
 
-    it("subtracts numbers", () => {
+    it("subtracts numbers", async () => {
         let ast = binary(new BrsTypes.Int32(2), Lexeme.Minus, new BrsTypes.Float(1.5));
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(0.5);
     });
 
-    it("multiplies numbers", () => {
+    it("multiplies numbers", async () => {
         let ast = binary(new BrsTypes.Int32(2), Lexeme.Star, new BrsTypes.Float(1.5));
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(3);
     });
 
-    it("divides numbers", () => {
+    it("divides numbers", async () => {
         let ast = binary(new BrsTypes.Int32(2), Lexeme.Slash, new BrsTypes.Float(1.5));
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBeCloseTo(1.33333, 5);
     });
 
-    it("integer-divides numbers", () => {
+    it("integer-divides numbers", async () => {
         let ast = binary(new BrsTypes.Int32(2), Lexeme.Backslash, new BrsTypes.Float(1.5));
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(1);
     });
 
-    it("modulos numbers", () => {
+    it("modulos numbers", async () => {
         let ast = binary(new BrsTypes.Int32(2), Lexeme.Mod, new BrsTypes.Float(1.5));
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(0.5);
     });
 
-    it("exponentiates numbers", () => {
+    it("exponentiates numbers", async () => {
         let ast = binary(new BrsTypes.Int32(2), Lexeme.Caret, new BrsTypes.Float(3));
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(8);
     });
 
-    it("follows arithmetic order-of-operations (PEMDAS)", () => {
+    it("follows arithmetic order-of-operations (PEMDAS)", async () => {
         // (6 + 5) * 4 - 3 ^ 2
         let ast = new Stmt.Expression(
             new Expr.Binary(
@@ -86,11 +86,11 @@ describe("interpreter arithmetic", () => {
             )
         );
 
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(35);
     });
 
-    it("doesn't allow non-numeric negation", () => {
+    it("doesn't allow non-numeric negation", async () => {
         let ast = new Stmt.Expression(
             new Expr.Unary(
                 token(Lexeme.Minus),
@@ -98,12 +98,12 @@ describe("interpreter arithmetic", () => {
             )
         );
 
-        let [ result ] = interpreter.exec([ast]);
+        let [ result ] = await interpreter.exec([ast]);
         expect(BrsError.found()).toBe(true);
         expect(result).toBe(BrsTypes.BrsInvalid.Instance);
     });
 
-    it("doesn't allow mixed-type arithmetic", () => {
+    it("doesn't allow mixed-type arithmetic", async () => {
         let ast = new Stmt.Expression(
             new Expr.Binary(
                 new Expr.Literal(new BrsTypes.Int32(3)),
@@ -112,11 +112,11 @@ describe("interpreter arithmetic", () => {
             )
         );
 
-        expect(() => interpreter.exec([ast])).toThrow(/Attempting to add non-homogeneous values/);
+        await expect(interpreter.exec([ast])).rejects.toThrow(/Attempting to add non-homogeneous values/);
         expect(BrsError.found()).toBe(true);
     });
 
-    it("bitwise ANDs integers", () => {
+    it("bitwise ANDs integers", async () => {
         // 110 AND 101 = 100
         // (6)     (5) = (4)
         let ast = new Stmt.Expression(
@@ -127,11 +127,11 @@ describe("interpreter arithmetic", () => {
             )
         );
 
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(4);
     });
 
-    it("bitwise ORs integers", () => {
+    it("bitwise ORs integers", async () => {
         // 110 OR 011 = 111
         // (6)    (3) = (7)
         let ast = new Stmt.Expression(
@@ -142,7 +142,7 @@ describe("interpreter arithmetic", () => {
             )
         );
 
-        let [result] = interpreter.exec([ast]);
+        let [result] = await interpreter.exec([ast]);
         expect(result.getValue()).toBe(7);
     });
 });

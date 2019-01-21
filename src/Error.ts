480eab0e0eb1df64f07ceffa2e85f86f7be2be03
Sean Barag <sean@barag.org>
2019-01-20 16:50:39 -0800

    chore(lexer): Make Lexer a class
    BREAKING-CHANGE: A static module is really useful if there's only ever
    one Lexer at a time, but having multiple Lexers gets pretty tricky to
    manage.  Wrapping that up in a class makes managing multiple instances
    nice and easy.

--- src/Error.ts
+++ src/Error.ts
@@ -4,10 +4,6 @@ let foundError = false;
 
 export function make(message: string, line: number, file?: string) {
     foundError = true;
-    return runtime(message, line, file);
-}
-
-export function runtime(message: string, line: number, file?: string) {
     let location = file ? `${file}: ${line}` : `Line ${line}`;
     let error = new Error(`[${location}] ${message}`);
     if (process.env.NODE_ENV !== "test") {

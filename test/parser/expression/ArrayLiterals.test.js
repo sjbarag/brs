ef22bc2791a980049dda520daa4fd80862e0cabb
Sean Barag <sean@barag.org>
2019-02-18 10:51:16 -0800

    test(parse): test location tracking for array literals

--- test/parser/expression/ArrayLiterals.test.js
+++ test/parser/expression/ArrayLiterals.test.js
@@ -189,4 +189,152 @@ describe("parser array literals", () => {
             expect(statements).toMatchSnapshot();
         });
     });
+
+    test("location tracking", () => {
+        /**
+         *    0   0   0   1
+         *    0   4   8   2
+         *  +--------------
+         * 1| a = [   ]
+         * 2|
+         * 3| b = [
+         * 4|
+         * 5|
+         * 6| ]
+         */
+        let { statements, errors } = parser.parse([
+            {
+                kind: Lexeme.Identifier,
+                text: "a",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 0 },
+                    end: { line: 1, column: 1 }
+                }
+            },
+            {
+                kind: Lexeme.Equal,
+                text: "=",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 2 },
+                    end: { line: 1, column: 3 }
+                }
+            },
+            {
+                kind: Lexeme.LeftSquare,
+                text: "[",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 4 },
+                    end: { line: 1, column: 5 }
+
+                }
+            },
+            {
+                kind: Lexeme.RightSquare,
+                text: "]",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 8 },
+                    end: { line: 1, column: 9 }
+                }
+            },
+            {
+                kind: Lexeme.Newline,
+                text: "\n",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 9 },
+                    end: { line: 1, column: 10 },
+                }
+            },
+            {
+                kind: Lexeme.Newline,
+                text: "\n",
+                isReserved: false,
+                location: {
+                    start: { line: 2, column: 0 },
+                    end: { line: 2, column: 1 },
+                }
+            },
+            {
+                kind: Lexeme.Identifier,
+                text: "b",
+                isReserved: false,
+                location: {
+                    start: { line: 3, column: 0 },
+                    end: { line: 3, column: 1 }
+                }
+            },
+            {
+                kind: Lexeme.Equal,
+                text: "=",
+                isReserved: false,
+                location: {
+                    start: { line: 3, column: 2 },
+                    end: { line: 3, column: 3 }
+                }
+            },
+            {
+                kind: Lexeme.LeftSquare,
+                text: "[",
+                isReserved: false,
+                location: {
+                    start: { line: 3, column: 4 },
+                    end: { line: 3, column: 5 }
+
+                }
+            },
+            {
+                kind: Lexeme.Newline,
+                text: "\n",
+                isReserved: false,
+                location: {
+                    start: { line: 4, column: 0 },
+                    end: { line: 4, column: 1 },
+                }
+            },
+            {
+                kind: Lexeme.Newline,
+                text: "\n",
+                isReserved: false,
+                location: {
+                    start: { line: 5, column: 0 },
+                    end: { line: 5, column: 1 },
+                }
+            },
+            {
+                kind: Lexeme.RightSquare,
+                text: "]",
+                isReserved: false,
+                location: {
+                    start: { line: 6, column: 0 },
+                    end: { line: 6, column: 1 }
+                }
+            },
+            {
+                kind: Lexeme.Eof,
+                text: "\0",
+                isReserved: false,
+                location: {
+                    start: { line: 6, column: 1 },
+                    end: { line: 6, column: 2 }
+                }
+            }
+        ]);
+
+        expect(errors).toEqual([]);
+        expect(statements.length).toEqual(2);
+        expect(statements.map(s => s.value.location)).toEqual([
+            {
+                start: { line: 1, column: 4 },
+                end: { line: 1, column: 9 }
+            },
+            {
+                start: { line: 3, column: 4 },
+                end: { line: 6, column: 1 }
+            }
+        ]);
+    });
 });

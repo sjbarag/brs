a7e0fa6ebabef0fb20b3a380c139ae22c923e728
Sean Barag <sean@barag.org>
2019-02-18 14:24:57 -0800

    test(parse): test location tracking for dotted and bracketed indexing

--- test/parser/expression/Indexing.test.js
+++ test/parser/expression/Indexing.test.js
@@ -44,6 +44,149 @@ describe("parser indexing", () => {
             expect(statements).not.toBeNull();
             expect(statements).toMatchSnapshot();
         });
+
+        test("location tracking", () => {
+            /**
+             *    0   0   0   1
+             *    0   4   8   2
+             *  +--------------
+             * 1| a = foo.bar
+             * 2| b = foo[2]
+             */
+            let { statements, errors } = parser.parse([
+                {
+                    kind: Lexeme.Identifier,
+                    text: "a",
+                    isReserved: false,
+                    location: {
+                        start: { line: 1, column: 0 },
+                        end: { line: 1, column: 1 }
+                    }
+                },
+                {
+                    kind: Lexeme.Equal,
+                    text: "=",
+                    isReserved: false,
+                    location: {
+                        start: { line: 1, column: 2 },
+                        end: { line: 1, column: 3 },
+                    }
+                },
+                {
+                    kind: Lexeme.Identifier,
+                    text: "foo",
+                    isReserved: false,
+                    location: {
+                        start: { line: 1, column: 4 },
+                        end: { line: 1, column: 7 }
+                    }
+                },
+                {
+                    kind: Lexeme.Dot,
+                    text: ".",
+                    isReserved: false,
+                    location: {
+                        start: { line: 1, column: 7 },
+                        end: { line: 1, column: 8 },
+                    }
+                },
+                {
+                    kind: Lexeme.Identifier,
+                    text: "bar",
+                    isReserved: false,
+                    location: {
+                        start: { line: 1, column: 8 },
+                        end: { line: 1, column: 11 }
+                    }
+                },
+                {
+                    kind: Lexeme.Newline,
+                    text: "\n",
+                    isReserved: false,
+                    location: {
+                        start: { line: 1, column: 11 },
+                        end: { line: 1, column: 12 }
+                    }
+                },
+                {
+                    kind: Lexeme.Identifier,
+                    text: "b",
+                    isReserved: false,
+                    location: {
+                        start: { line: 2, column: 0 },
+                        end: { line: 2, column: 1 }
+                    }
+                },
+                {
+                    kind: Lexeme.Equal,
+                    text: "=",
+                    isReserved: false,
+                    location: {
+                        start: { line: 2, column: 2 },
+                        end: { line: 2, column: 3 },
+                    }
+                },
+                {
+                    kind: Lexeme.Identifier,
+                    text: "bar",
+                    isReserved: false,
+                    location: {
+                        start: { line: 2, column: 4 },
+                        end: { line: 2, column: 7 }
+                    }
+                },
+                {
+                    kind: Lexeme.LeftSquare,
+                    text: "[",
+                    isReserved: false,
+                    location: {
+                        start: { line: 2, column: 7 },
+                        end: { line: 2, column: 8 }
+                    }
+                },
+                {
+                    kind: Lexeme.Integer,
+                    text: "2",
+                    literal: new Int32(2),
+                    isReserved: false,
+                    location: {
+                        start: { line: 2, column: 8 },
+                        end: { line: 2, column: 9 }
+                    }
+                },
+                {
+                    kind: Lexeme.RightSquare,
+                    text: "]",
+                    isReserved: false,
+                    location: {
+                        start: { line: 2, column: 9 },
+                        end: { line: 2, column: 10 }
+                    }
+                },
+                {
+                    kind: Lexeme.Eof,
+                    text: "\0",
+                    isReserved: false,
+                    location: {
+                        start: { line: 2, column: 10 },
+                        end: { line: 2, column: 11 }
+                    }
+                }
+            ]);
+
+            expect(errors).toEqual([]);
+            expect(statements.length).toBe(2);
+            expect(statements.map(s => s.value.location)).toEqual([
+                {
+                    start: { line: 1, column: 4 },
+                    end: { line: 1, column: 11 }
+                },
+                {
+                    start: { line: 2, column: 4 },
+                    end: { line: 2, column: 10 }
+                }
+            ]);
+        });
     });
 
     describe("multi-level", () => {

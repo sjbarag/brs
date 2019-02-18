69809df4bc130c47d57325ad3e340229cbccc277
Sean Barag <sean@barag.org>
2019-02-18 15:39:40 -0800

    test(parse): test location tracking for return statements

--- test/parser/statement/ReturnStatement.test.js
+++ test/parser/statement/ReturnStatement.test.js
@@ -1,6 +1,6 @@
 const brs = require("brs");
 const { Lexeme } = brs.lexer;
-const { BrsString } = brs.types;
+const { Int32, BrsString } = brs.types;
 
 const { token, identifier, EOF } = require("../ParserTests");
 
@@ -70,5 +70,51 @@ describe("parser return statements", () => {
         expect(statements).toBeDefined();
         expect(statements).not.toBeNull();
         expect(statements).toMatchSnapshot();
+    });
+
+    test("location tracking", () => {
+        /**
+         *    0   0   0   1   1
+         *    0   4   8   2   6
+         *  +------------------
+         * 1| function foo()
+         * 2|   return 5
+         * 3| end function
+         */
+        let { statements, errors } = parser.parse([
+            token(Lexeme.Function, "function"),
+            identifier("foo"),
+            token(Lexeme.LeftParen, "("),
+            token(Lexeme.RightParen, ")"),
+            token(Lexeme.Newline, "\\n"),
+            {
+                kind: Lexeme.Return,
+                text: "return",
+                isReserved: true,
+                location: {
+                    start: { line: 2, column: 2 },
+                    end: { line: 2, column: 8 }
+                }
+            },
+            {
+                kind: Lexeme.Integer,
+                text: "5",
+                literal: new Int32(5),
+                isReserved: false,
+                location: {
+                    start: { line: 2, column: 9 },
+                    end: { line: 2, column: 10 }
+                }
+            },
+            token(Lexeme.Newline, "\\n"),
+            token(Lexeme.EndFunction, "end function"),
+            EOF
+        ]);
+
+        expect(errors).toEqual([]);
+        expect(statements[0].func.body.statements[0].location).toEqual({
+            start: { line: 2, column: 2 },
+            end: { line: 2, column: 10 }
+        });
     });
 });

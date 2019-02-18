440bb6255ded356b3e68f506b291daa2431bd706
Sean Barag <sean@barag.org>
2019-02-18 12:08:06 -0800

    test(parse): test function call location tracking

--- test/parser/expression/Call.test.js
+++ test/parser/expression/Call.test.js
@@ -1,6 +1,6 @@
 const brs = require("brs");
 const { Lexeme } = brs.lexer;
-const { Int32 } = brs.types;
+const { Int32, BrsString } = brs.types;
 
 const { token, identifier, EOF } = require("../ParserTests");
 
@@ -57,5 +57,88 @@ describe("parser call expressions", () => {
         expect(statements).not.toBeNull();
         expect(statements[0].expression.args).toBeTruthy();
         expect(statements).toMatchSnapshot();
+    });
+
+    test("location tracking", () => {
+        /**
+         *    0   0   0   1   1   2
+         *    0   4   8   2   6   0
+         *  +----------------------
+         * 1| foo("bar", "baz")
+         */
+        const { statements, errors } = parser.parse([
+            {
+                kind: Lexeme.Identifier,
+                text: "foo",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 0 },
+                    end: { line: 1, column: 3 },
+                }
+            },
+            {
+                kind: Lexeme.LeftParen,
+                text: "(",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 3 },
+                    end: { line: 1, column: 4 }
+                }
+            },
+            {
+                kind: Lexeme.String,
+                text: `"bar"`,
+                literal: new BrsString("bar"),
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 4 },
+                    end: { line: 1, column: 9 }
+                }
+            },
+            {
+                kind: Lexeme.Comma,
+                text: ",",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 9 },
+                    end: { line: 1, column: 10 }
+                }
+            },
+            {
+                kind: Lexeme.String,
+                text: `"baz"`,
+                literal: new BrsString("baz"),
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 11 },
+                    end: { line: 1, column: 16 }
+                }
+            },
+            {
+                kind: Lexeme.RightParen,
+                text: ")",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 16 },
+                    end: { line: 1, column: 17 }
+                }
+            },
+            {
+                kind: Lexeme.Eof,
+                text: "\0",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 17 },
+                    end: { line: 1, column: 18 }
+                }
+            }
+        ]);
+
+        expect(errors).toEqual([]);
+        expect(statements.length).toBe(1);
+        expect(statements[0].location).toEqual({
+            start: { line: 1, column: 0 },
+            end: { line: 1, column: 17 }
+        });
     });
 });

12cfd6d20bf3b82c387ef234b92487178272b3ff
Sean Barag <sean@barag.org>
2019-02-20 08:18:05 -0800

    test(parse): Test for-each loop location tracking

--- test/parser/controlFlow/ForEach.test.js
+++ test/parser/controlFlow/ForEach.test.js
@@ -62,4 +62,81 @@ describe("parser foreach loops", () => {
         expect(statements).toBeDefined();
         expect(statements).toMatchSnapshot();
     });
+
+    test("location tracking", () => {
+        /**
+         *    0   0   0   1   1
+         *    0   4   8   2   6
+         *  +------------------
+         * 1| for each a in b
+         * 2|   Rnd(a)
+         * 3| end for
+         */
+        let { statements, errors } = parser.parse([
+            {
+                kind: Lexeme.ForEach,
+                text: "for each",
+                isReserved: true,
+                location: {
+                    start: { line: 1, column: 0 },
+                    end: { line: 1, column: 8 }
+                }
+            },
+            {
+                kind: Lexeme.Identifier,
+                text: "a",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 9 },
+                    end: { line: 1, column: 10 }
+                }
+            },
+            {
+                kind: Lexeme.Identifier,
+                text: "in",
+                isReserved: true,
+                location: {
+                    start: { line: 1, column: 11 },
+                    end: { line: 1, column: 13 }
+                }
+            },
+            {
+                kind: Lexeme.Identifier,
+                text: "b",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 14 },
+                    end: { line: 1, column: 15 }
+                }
+            },
+            {
+                kind: Lexeme.Newline,
+                text: "\n",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 15 },
+                    end: { line: 1, column: 16 }
+                }
+            },
+            // loop body isn't significant for location tracking, so helper functions are safe
+            identifier("Rnd"), token(Lexeme.LeftParen, "("), identifier("a"), token(Lexeme.RightParen, ")"), token(Lexeme.Newline, "\n"),
+            {
+                kind: Lexeme.EndFor,
+                text: "end for",
+                isReserved: false,
+                location: {
+                    start: { line: 3, column: 0 },
+                    end: { line: 3, column: 7 }
+                }
+            },
+            EOF
+        ]);
+
+        expect(errors).toEqual([]);
+        expect(statements.length).toBe(1);
+        expect(statements[0].location).toEqual({
+            start: { line: 1, column: 0 },
+            end: { line: 3, column: 7 }
+        })
+    });
 });

18287f67d06c3056438613ce055cd02f871c2690
Sean Barag <sean@barag.org>
2019-02-20 08:26:20 -0800

    test(parser): Test while loop location tracking

--- test/parser/controlFlow/While.test.js
+++ test/parser/controlFlow/While.test.js
@@ -1,6 +1,6 @@
 const brs = require("brs");
 const { Lexeme } = brs.lexer;
-const { BrsBoolean, BrsString } = brs.types;
+const { BrsBoolean, BrsString, Int32 } = brs.types;
 const { token, identifier, EOF } = require("../ParserTests");
 
 describe("parser while statements", () => {
@@ -46,5 +46,71 @@ describe("parser while statements", () => {
         expect(statements).toBeDefined();
         expect(statements).not.toBeNull();
         expect(statements).toMatchSnapshot();
+    });
+
+    test("location tracking", () => {
+        /**
+         *    0   0   0   1   1
+         *    0   4   8   2   6
+         *  +------------------
+         * 1| while true
+         * 2|   Rnd(0)
+         * 3| end while
+         */
+        const { statements, errors } = parser.parse([
+            {
+                kind: Lexeme.While,
+                text: "while",
+                isReserved: true,
+                location: {
+                    start: { line: 1, column: 0 },
+                    end: { line: 1, column: 5 }
+                }
+            },
+            {
+                kind: Lexeme.True,
+                text: "true",
+                literal: BrsBoolean.True,
+                isReserved: true,
+                location: {
+                    start: { line: 1, column: 6 },
+                    end: { line: 1, column: 10 }
+                }
+            },
+            {
+                kind: Lexeme.Newline,
+                text: "\n",
+                isReserved: false,
+                location: {
+                    start: { line: 1, column: 10 },
+                    end: { line: 1, column: 11 }
+                }
+            },
+            // loop body isn't significant for location tracking, so helper functions are safe
+            identifier("Rnd"),
+            token(Lexeme.LeftParen, "("),
+            token(Lexeme.Integer, "0", new Int32(0)),
+            token(Lexeme.RightParen, ")"),
+            token(Lexeme.Newline, "\n"),
+
+            {
+                kind: Lexeme.EndWhile,
+                text: "end while",
+                isReserved: false,
+                location: {
+                    start: { line: 3, column: 0 },
+                    end: { line: 3, column: 9 }
+                }
+            },
+            EOF
+        ]);
+
+        expect(errors).toEqual([]);
+        expect(statements.length).toBe(1);
+        expect(statements[0].location).toEqual({
+            start: { line: 1, column: 0 },
+            end: { line: 3, column: 9 }
+        });
+
     });
 });

25b6c8c097f2232c3f02d532978ad836246b02ea
Bronley Plumb <bronley@gmail.com>
2019-03-18 23:32:10 -0400

    Removed console.log statements

--- test/lexer/Lexer.test.js
+++ test/lexer/Lexer.test.js
@@ -345,7 +345,6 @@ describe("lexer", () => {
     describe('two word keywords', () => {
         it('supports various spacing between for each', () => {
             let { tokens } = Lexer.scan('for each for  each for    each for\teach for\t each for \teach for \t each');
-            console.log(tokens);
             expect(tokens.map(t => t.kind)).toEqual([
                 Lexeme.ForEach,
                 Lexeme.ForEach,
@@ -359,7 +358,6 @@ describe("lexer", () => {
         });
         it('supports various spacing between else if', () => {
             let { tokens } = Lexer.scan('else if else  if else    if else\tif else\t if else \tif else \t if');
-            console.log(tokens);
             expect(tokens.map(t => t.kind)).toEqual([
                 Lexeme.ElseIf,
                 Lexeme.ElseIf,

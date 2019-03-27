b7475edf1ebf5b9ca5c28b7bafdb12063fdb06bb
Jorge Zapata <alimnios72@users.noreply.github.com>
2019-03-27 12:50:01 -0700

    feat(stdlib): Implement createObject("roRegex") (#219)
    This feature implements all methods of the
    [ifRegex](https://sdkdocs.roku.com/display/sdkdoc/ifRegex) interface.
    Although javascript's `RegExp` doesn't fully support PCRE, as
    Brightscript does, it gives `brs` the ability to support most of the
    common regex use cases.
    
    All flags in [roRegex](https://sdkdocs.roku.com/display/sdkdoc/roRegex)
    but "x" should be supported.

---
+++ src/brsTypes/components/Regex.ts
@@ -0,0 +1,188 @@
+import { BrsBoolean, BrsInvalid, BrsString, BrsValue, ValueKind } from "../BrsType";
+import { BrsComponent } from "./BrsComponent";
+import { BrsType } from "..";
+import { Callable, StdlibArgument } from "../Callable";
+import { Interpreter } from "../../interpreter";
+import { BrsArray } from "./BrsArray";
+
+export class Regex extends BrsComponent implements BrsValue {
+    readonly kind = ValueKind.Object;
+    readonly supportedFlags = "ims";
+    private jsRegex : RegExp;
+
+    constructor(expression: BrsString, flags = new BrsString("")) {
+        super("roRegex");
+        this.jsRegex = new RegExp(expression.value, this.parseFlags(flags.value));
+
+        this.registerMethods([
+            this.isMatch,
+            this.match,
+            this.replace,
+            this.replaceAll,
+            this.split,
+            this.matchAll
+        ]);
+    }
+
+    toString(parent?: BrsType): string {
+        return "<Component: roRegex>";
+    }
+
+    /**
+     * Checks and parses the flags to avoid passing flags
+     * that are not supported
+     * @param inputFlags Flags passed to constructor
+     * @returns parsed flags
+     */
+    private parseFlags(inputFlags: string): string {
+        let parsedFlags = "";
+        if (inputFlags.length === 0) {
+            return parsedFlags;
+        }
+
+        for (const flag of inputFlags) {
+            if (flag === "x") {
+                console.warn("'x' flag is not implemented yet, ignoring flag.");
+            } else if(this.supportedFlags.indexOf(flag) === -1) {
+                throw new Error(`${flag} is not supported.`);
+            } else {
+                parsedFlags += flag;
+            }
+        }
+
+        return parsedFlags;
+    }
+
+    /**
+     * Transforms positional pattern replacements to javascript syntax
+     * by replacing backslashes with dollar symbols
+     * @param pattern Pattern to replace
+     * @returns Replaced string
+     */
+    private parseReplacementPattern(pattern: string): string {
+        return pattern.replace(/\\/g, "\$");
+    }
+
+    /** Returns whether the string matched the regex or not */
+    private isMatch = new Callable(
+        "ismatch",
+        {
+            signature: {
+                args: [
+                    new StdlibArgument("str", ValueKind.String)
+                ],
+                returns: ValueKind.Boolean
+            },
+            impl: (interpreter: Interpreter, str: BrsString) => {
+                return BrsBoolean.from(this.jsRegex.test(str.value));
+            }
+        }
+    );
+
+    /** Returns an array of matches */
+    private match = new Callable(
+        "match",
+        {
+            signature: {
+                args: [
+                    new StdlibArgument("str", ValueKind.String)
+                ],
+                returns: ValueKind.Object
+            },
+            impl: (interpreter: Interpreter, str: BrsString) => {
+                const result = this.jsRegex.exec(str.value);
+                let arr: (BrsString | BrsInvalid)[] = [];
+                if (result !== null) {
+                    arr = result.map(match => match ? new BrsString(match) : BrsInvalid.Instance);
+                }
+
+                return new BrsArray(arr);
+            }
+        }
+    );
+
+    /** Returns a new string with first match replaced */
+    private replace = new Callable(
+        "replace",
+        {
+            signature: {
+                args: [
+                    new StdlibArgument("str", ValueKind.String),
+                    new StdlibArgument("replacement", ValueKind.String)
+                ],
+                returns: ValueKind.String
+            },
+            impl: (interpreter: Interpreter, str: BrsString, replacement: BrsString) => {
+                let replacementPattern = this.parseReplacementPattern(replacement.value);
+                const newStr = this.jsRegex[Symbol.replace](str.value, replacementPattern);
+                return new BrsString(newStr);
+            }
+        }
+    );
+
+    /** Returns a new string with all matches replaced */
+    private replaceAll = new Callable(
+        "replaceall",
+        {
+            signature: {
+                args: [
+                    new StdlibArgument("str", ValueKind.String),
+                    new StdlibArgument("replacement", ValueKind.String)
+                ],
+                returns: ValueKind.String
+            },
+            impl: (interpreter: Interpreter, str: BrsString, replacement: BrsString) => {
+                const source = this.jsRegex.source;
+                const flags = this.jsRegex.flags + "g";
+                this.jsRegex = new RegExp(source, flags);
+                const newStr = this.jsRegex[Symbol.replace](str.value, replacement.value);
+
+                return new BrsString(newStr);
+            }
+        }
+    );
+
+    /** Returns an array of strings split by match */
+    private split = new Callable(
+        "split",
+        {
+            signature: {
+                args: [
+                    new StdlibArgument("str", ValueKind.String)
+                ],
+                returns: ValueKind.Object
+            },
+            impl: (interpreter: Interpreter, str: BrsString) => {
+                let items = this.jsRegex[Symbol.split](str.value);
+                let brsItems = items.map( item => new BrsString(item));
+                return new BrsArray(brsItems);
+            }
+        }
+    );
+
+    /** Returns an array of array with all matches found */
+    private matchAll = new Callable(
+        "matchall",
+        {
+            signature: {
+                args: [
+                    new StdlibArgument("str", ValueKind.String)
+                ],
+                returns: ValueKind.Object
+            },
+            impl: (interpreter: Interpreter, str: BrsString) => {
+                const source = this.jsRegex.source;
+                const flags = this.jsRegex.flags + "g";
+                this.jsRegex = new RegExp(source, flags);
+                let arr = [];
+                let matches: string[] | null;
+
+                while ((matches = this.jsRegex.exec(str.value)) !== null) {
+                    let item = matches[0] ? new BrsString(matches[0]) : BrsInvalid.Instance;
+                    arr.push(new BrsArray([ item ]));
+                }
+                return new BrsArray(arr);
+            }
+        }
+    );
+}

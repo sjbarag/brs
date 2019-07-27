4a86bd34a6823fa14c36eb52e24c33986bed7683
Marcelo Lv Cabral <lvcabral@hotmail.com>
2019-07-26 21:46:43 -0700

    Implemented roDateTime component

--- src/brsTypes/components/BrsObjects.ts
+++ src/brsTypes/components/BrsObjects.ts
@@ -5,11 +5,13 @@ import { RoSGNode, createNodeByType } from "./RoSGNode";
 import { RoRegex } from "./RoRegex";
 import { BrsString } from "../BrsType";
 import { RoString } from "./RoString";
+import { RoDateTime } from "./RoDateTime";
 
 /** Map containing a list of brightscript components that can be created. */
 export const BrsObjects = new Map<string, Function>([
     ["roassociativearray", () => new RoAssociativeArray([])],
     ["roarray", () => new RoArray([])],
+    ["rodatetime", () => new RoDateTime()],
     ["rotimespan", () => new Timespan()],
     ["rosgnode", (nodeType: BrsString) => createNodeByType(nodeType)],
     ["roregex", (expression: BrsString, flags: BrsString) => new RoRegex(expression, flags)],

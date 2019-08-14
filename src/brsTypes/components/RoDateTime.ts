edf9ca4d1bd48646306737fa19ed429805a4edce
Williams Martinez <williams.martinez@hulu.com>
2019-08-13 20:26:34 -0500

    fromSeconds, toISOString, fromISO8601String and asDateString implementation

--- src/brsTypes/components/RoDateTime.ts
+++ src/brsTypes/components/RoDateTime.ts
@@ -4,10 +4,23 @@ import { BrsType } from "..";
 import { Callable, StdlibArgument } from "../Callable";
 import { Interpreter } from "../../interpreter";
 import { Int32 } from "../Int32";
+import * as luxon from "luxon";
+import * as dateFns from "date-fns";
 
 export class RoDateTime extends BrsComponent implements BrsValue, Comparable {
     readonly kind = ValueKind.Object;
     private dateTime: Date;
+
+    private formatOptions: { [id: string]: string } = {
+        "long-date": "dddd MMMM D, YYYY",
+        "short-weekday": "dd MMMM D, YYYY",
+        "no-weekday": "MMMM D, YYYY",
+        "short-month": "dddd MMM D, YYYY",
+        "short-month-short-weekday": "dd MMM D, YYYY",
+        "short-month-no-weekday": "MMM D, YYYY",
+        "short-date": "M/D/YY",
+        "short-date-dashes": "M-D-YY",
+    };
 
     constructor() {
         super("roDateTime", ["ifDateTime"]);
@@ -18,11 +31,11 @@ export class RoDateTime extends BrsComponent implements BrsValue, Comparable {
             this.toLocalTime,
             this.getTimeZoneOffset,
             this.asSeconds,
-            /** 
             this.fromSeconds,
             this.toISOString,
             this.fromISO8601String,
             this.asDateString,
+            /*
             this.asDateStringNoParam,
             this.getWeekday,
             this.getYear,
@@ -54,10 +67,6 @@ export class RoDateTime extends BrsComponent implements BrsValue, Comparable {
         return BrsBoolean.False;
     }
 
-    unbox() {
-        return this.dateTime;
-    }
-
     toString(parent?: BrsType): string {
         return "<Component: roDateTime>";
     }
@@ -79,6 +88,7 @@ export class RoDateTime extends BrsComponent implements BrsValue, Comparable {
             returns: ValueKind.Invalid,
         },
         impl: (interpreter: Interpreter) => {
+            /** TODO: Implement toLocalTime */
             return BrsInvalid.Instance;
         },
     });
@@ -100,6 +110,62 @@ export class RoDateTime extends BrsComponent implements BrsValue, Comparable {
         },
         impl: (interpreter: Interpreter) => {
             return new Int32(this.dateTime.getTime() / 1000);
+        },
+    });
+
+    private fromSeconds = new Callable("fromSeconds", {
+        signature: {
+            args: [new StdlibArgument("numSeconds", ValueKind.Int32)],
+            returns: ValueKind.Invalid,
+        },
+        impl: (_: Interpreter, seconds: Int32) => {
+            let milliseconds = seconds.getValue() * 1000;
+            this.dateTime = new Date(milliseconds);
+
+            return BrsInvalid.Instance;
+        },
+    });
+
+    private toISOString = new Callable("toISOString", {
+        signature: {
+            args: [],
+            returns: ValueKind.String,
+        },
+        impl: (interpreter: Interpreter) => {
+            return new BrsString(this.dateTime.toISOString());
+        },
+    });
+
+    private fromISO8601String = new Callable("fromISO8601String", {
+        signature: {
+            args: [new StdlibArgument("dateString", ValueKind.String)],
+            returns: ValueKind.Invalid,
+        },
+        impl: (_: Interpreter, dateString: BrsString) => {
+            let milliseconds = Date.parse(dateString.value);
+            if ((milliseconds = NaN)) {
+                this.dateTime = new Date();
+            } else {
+                this.dateTime = new Date(milliseconds);
+            }
+            return BrsInvalid.Instance;
+        },
+    });
+
+    private asDateString = new Callable("asDateString", {
+        signature: {
+            args: [new StdlibArgument("dateString", ValueKind.String)],
+            returns: ValueKind.String,
+        },
+        impl: (_: Interpreter, format: BrsString) => {
+            if (format.value in this.formatOptions) {
+                let formatter = this.formatOptions[format.value];
+
+                let dateString = dateFns.format(this.dateTime, formatter);
+                return new BrsString(dateString);
+            } else {
+                return new BrsString("");
+            }
         },
     });
 }
--- test/brsTypes/components/RoDateTime.test.js
+++ test/brsTypes/components/RoDateTime.test.js
@@ -2,12 +2,13 @@ const brs = require("brs");
 const { RoDateTime, Int32, BrsString, BrsInvalid } = brs.types;
 const { Interpreter } = require("../../../lib/interpreter");
 const lolex = require("lolex");
+const dateFns = require("date-fns");
 
 describe("RoDateTime", () => {
     let dateTime;
     let clock;
-    let fixedDate = "2019-08-09T20:52:58.655Z";
-    let fixedDateInMilliseconds = 1565383928655;
+    let fixedDate = "2019-08-09T20:52:58.000Z";
+    let fixedDateInMilliseconds = 1565383928000;
 
     beforeEach(() => {
         clock = lolex.install({ now: fixedDateInMilliseconds });
@@ -29,6 +30,7 @@ describe("RoDateTime", () => {
 
         beforeEach(() => {
             interpreter = new Interpreter();
+            dateTime = new RoDateTime();
         });
 
         describe("mark", () => {
@@ -66,6 +68,55 @@ describe("RoDateTime", () => {
                 expect(asSeconds).toBeTruthy();
                 expect(dateTime.dateTime).toEqual(Date());
                 expect(result).toEqual(new Int32(fixedDateInSeconds));
+            });
+        });
+
+        describe("fromSeconds", () => {
+            it("Set the date/time value using the number of seconds from the Unix epoch", () => {
+                let fromSeconds = dateTime.getMethod("fromSeconds");
+                let advanceTime = 50000;
+                clock.tick(advanceTime);
+                let fixedDateInSeconds = (fixedDateInMilliseconds + advanceTime) / 1000;
+                let result = fromSeconds.call(interpreter, new Int32(fixedDateInSeconds));
+
+                expect(fromSeconds).toBeTruthy();
+                expect(dateTime.dateTime).toEqual(Date());
+            });
+        });
+
+        describe("toISOString", () => {
+            it("Return an ISO 8601 representation of the date/time value", () => {
+                let toISOString = dateTime.getMethod("toISOString");
+                let result = toISOString.call(interpreter);
+                let expected = new BrsString(Date().toISOString());
+
+                expect(toISOString).toBeTruthy();
+                expect(result).toEqual(expected);
+            });
+        });
+
+        describe("asDateString", () => {
+            it("Returns the date/time as a formatted string", () => {
+                let asDateString = dateTime.getMethod("asDateString");
+                let formatOptions = {
+                    "long-date": "dddd MMMM D, YYYY",
+                    "short-weekday": "dd MMMM D, YYYY",
+                    "no-weekday": "MMMM D, YYYY",
+                    "short-month": "dddd MMM D, YYYY",
+                    "short-month-short-weekday": "dd MMM D, YYYY",
+                    "short-month-no-weekday": "MMM D, YYYY",
+                    "short-date": "M/D/YY",
+                    "short-date-dashes": "M-D-YY",
+                };
+
+                let formats = Object.keys(formatOptions);
+                formats.forEach(format => {
+                    let result = asDateString.call(interpreter, new BrsString(format));
+                    let expected = dateFns.format(Date(), formatOptions[format]);
+
+                    expect(asDateString).toBeTruthy();
+                    expect(result).toEqual(new BrsString(expected));
+                });
             });
         });
     });

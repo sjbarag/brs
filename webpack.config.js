44d1fe0d03028cbf8300a067dbef3db6a327eb67
Marcelo L Cabral <mcabral@mmsi.com>
2019-08-14 18:25:43 -0700

    Added new package script to build release version

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,44 @@
 const path = require("path");
 
-const isProduction = typeof NODE_ENV !== "undefined" && NODE_ENV === "production";
-let outputFile, mode;
-let libraryName = "brsLib";
-if (isProduction) {
-    mode = "production";
-    outputFile = libraryName + ".min.js";
-} else {
-    mode = "development";
-    outputFile = libraryName + ".js";
-}
-module.exports = [
-    {
-        entry: "./src/index.ts",
-        target: "web",
-        mode: mode,
-        devtool: "inline-source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    loader: "ts-loader",
-                    exclude: /node_modules/,
-                },
-            ],
+module.exports = env => {
+    const isProduction = env.NODE_ENV === "production";
+    let outputFile, mode;
+    let libraryName = "brsLib";
+    if (isProduction) {
+        mode = "production";
+        outputFile = libraryName + ".min.js";
+    } else {
+        mode = "development";
+        outputFile = libraryName + ".js";
+    }
+    return [
+        {
+            entry: "./src/index.ts",
+            target: "web",
+            mode: mode,
+            devtool: "inline-source-map",
+            module: {
+                rules: [
+                    {
+                        test: /\.tsx?$/,
+                        loader: "ts-loader",
+                        exclude: /node_modules/,
+                    },
+                ],
+            },
+            resolve: {
+                modules: [path.resolve("./node_modules"), path.resolve("./src")],
+                extensions: [".tsx", ".ts", ".js"],
+            },
+            node: { fs: "empty", readline: "empty" },
+            output: {
+                path: path.join(__dirname, "lib"),
+                filename: outputFile,
+                library: libraryName,
+                libraryTarget: "umd",
+                umdNamedDefine: true,
+                globalObject: "typeof self !== 'undefined' ? self : this",
+            },
         },
-        resolve: {
-            modules: [path.resolve("./node_modules"), path.resolve("./src")],
-            extensions: [".tsx", ".ts", ".js"],
-        },
-        node: { fs: "empty", readline: "empty" },
-        output: {
-            path: path.join(__dirname, "lib"),
-            filename: outputFile,
-            library: libraryName,
-            libraryTarget: "umd",
-            umdNamedDefine: true,
-            globalObject: "typeof self !== 'undefined' ? self : this",
-        },
-    },
-];
+    ];
+};

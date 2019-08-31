20593eaf72909dba985fc5bc999cea4ac169db72
Marcelo L Cabral <mcabral@mmsi.com>
2019-08-30 18:02:04 -0700

    Implemented basic funcionality of roByteArray #281

--- src/brsTypes/components/BrsObjects.ts
+++ src/brsTypes/components/BrsObjects.ts
@@ -5,11 +5,13 @@ import { RoSGNode, createNodeByType } from "./RoSGNode";
 import { RoRegex } from "./RoRegex";
 import { BrsString } from "../BrsType";
 import { RoString } from "./RoString";
+import { RoByteArray } from "./RoByteArray";
 
 /** Map containing a list of brightscript components that can be created. */
 export const BrsObjects = new Map<string, Function>([
     ["roassociativearray", () => new RoAssociativeArray([])],
     ["roarray", () => new RoArray([])],
+    ["robytearray", () => new RoByteArray()],
     ["rotimespan", () => new Timespan()],
     ["rosgnode", (nodeType: BrsString) => createNodeByType(nodeType)],
     ["roregex", (expression: BrsString, flags: BrsString) => new RoRegex(expression, flags)],

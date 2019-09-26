32e3f89e8dd28cbaecd8bd52b361c70bbbcf252e
Marcelo Lv Cabral <marcelo.cabral@gmail.com>
2019-09-26 12:07:52 -0700

    fix(types,interp): Automatically convert between Int and Float (#291)
    Roku does truncate float to integer (not round) so I also changed the
    constructor of Int32 and Int64 to correctly mimic the behavior.
    
    fixes #280

--- test/brsTypes/Int64.test.js
+++ test/brsTypes/Int64.test.js
@@ -11,18 +11,14 @@ const {
 const Long = require("long");
 
 describe("Int64", () => {
-    it("rounds initial values to integers", () => {
-        let three = new Int64(3.4);
-        let four = new Int64(3.5);
-        expect(three.getValue().toNumber()).toBe(3);
-        expect(four.getValue().toNumber()).toBe(4);
+    it("truncates initial values to integers", () => {
+        let threePointNine = new Int64(3.9);
+        expect(threePointNine.getValue().toNumber()).toBe(3);
     });
 
     it("creates base-10 integers from strings", () => {
-        let three = Int64.fromString("3.4");
-        let four = Int64.fromString("3.5");
-        expect(three.getValue().toNumber()).toBe(3);
-        expect(four.getValue().toNumber()).toBe(4);
+        let threePointNine = Int64.fromString("3.9");
+        expect(threePointNine.getValue().toNumber()).toBe(3);
     });
 
     it("creates base-16 integers from strings", () => {

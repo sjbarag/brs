c57c458816501f025852f3f90c507dde43f906e8
Sean Barag <sean.barag@hulu.com>
2019-12-31 09:20:23 -0800

    fix(rsg): Prevent infinite loop when a node extends an unknown component
    Long-term we probably don't want to warn-and-fall-back for components
    that extend something we don't recognize.  The reference implementation
    considers that a compilation failure, but since we don't yet know about
    every component in the reference implementation we can't make that
    assertion!

--- src/componentprocessor/index.ts
+++ src/componentprocessor/index.ts
@@ -148,6 +148,14 @@ async function processXmlTree(
                 if (baseNodeDef) {
                     nodeDef.children = [...nodeDef.children];
                     baseNode = baseNodeDef.xmlNode!.attr.extends;
+                } else {
+                    // The reference implementation doesn't allow extensions of unknown node subtypes, but
+                    // BRS hasn't implemented every node type in the reference implementation!  For now,
+                    // let's warn when we detect unknown subtypes.
+                    console.error(
+                        `XML component '${nodeDef.name}' extends unknown component '${baseNode}'. Ignoring extension.`
+                    );
+                    break;
                 }
             }
         }

42d55a8d35a942211075f3620d4557ddd377e34f
Sean Barag <sean.barag@hulu.com>
2020-01-28 09:34:28 -0800

    feat(stdlib): Add _brs_.process.argv to mirror node's process.argv
    It's currently impossible to access `argv` from a BrightScript file
    executed with `brs`!  While it's also impossible via the reference
    BrightScript implementation, there's basically no concept of argv there
    -- instead they accept an associative array of arguments from the OS
    that implements RBI.
    
    Piping `process.argv` into `brs` should enable more interactive uses of
    BrightScript outside of the reference implementation.  While we _could_
    theoretically parse `argv` into an associative array similar to
    [minimist](https://github.com/substack/minimist)'s output, it's probably
    best to leave that to BrightScript implementations for now.

---
+++ test/e2e/resources/stdlib/_brs_-namespace.brs
@@ -0,0 +1,3 @@
+sub main()
+    print "_brs_.process.argv is non-empty: " _brs_.process.argv.count() > 0
+end sub
---
+++ test/extensions/Process.test.js
@@ -0,0 +1,11 @@
+const brs = require("brs");
+const { BrsString } = brs.types;
+const { Process: brsProcess } = require("../../lib/extensions/Process");
+
+describe("_brs_.process", () => {
+    it("mirrors node's process.argv", () => {
+        let brsArgv = brsProcess.get(new BrsString("argv"));
+        expect(brsArgv).toBeDefined();
+        expect(brsArgv.getValue().map(arg => arg.value)).toEqual(process.argv);
+    });
+});

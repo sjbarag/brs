52bbb3f5fffdd1915efb2ae78591e6a2a6dcb13f
Kirk Yoder <kirk.yoder@hulu.com>
2020-05-01 12:29:50 -0600

    add built in fields

--- src/brsTypes/components/Group.ts
+++ src/brsTypes/components/Group.ts
@@ -1,3 +1,23 @@
 import { RoSGNode } from "../../../lib/brsTypes/components/RoSGNode";
 
-export class Group extends RoSGNode {}
+export type RenderTracking = "none" | "partial" | "full";
+export type ChildRenderOrder = "renderFirst" | "renderLast";
+
+export class Group extends RoSGNode {
+    readonly builtInFields = [
+        { name: "visible", type: "boolean" },
+        { name: "opacity", type: "float" },
+        { name: "translation", type: "array" }, // 2D Vector [0.0, 0.0]
+        { name: "rotation", type: "float" },
+        { name: "scale", type: "array" }, // 2D Vector [0.0, 0.0]
+        { name: "scaleRotateCenter", type: "array" }, // 2D Vector [0.0, 0.0]
+        { name: "childRenderOrder", type: "string" }, // ChildRenderOrder
+        { name: "inheritParentTransform", type: "boolean" },
+        { name: "inheritParentOpacity", type: "boolean" },
+        { name: "clippingRect", type: "array" }, // [0.0, 0.0, 0.0, 0.0]
+        { name: "renderPass", type: "integer" },
+        { name: "muteAudioGuide", type: "boolean" },
+        { name: "enableRenderTracking", type: "boolean" },
+        { name: "renderTracking", type: "string" }, // RenderTracking
+    ];
+}

46fe42cbc928f4215a4edc867df45f1187614969
Nicco Simone <nicco.simone@hulu.com>
2020-09-23 17:14:31 -0700

    fix: minor naming collision with eslint/estree
    - Rename Function.end to Function.endKeyword
    
    AST nodes should not have a start or end property.
    https://github.com/eslint/eslint/pull/8984

--- src/parser/Expression.ts
+++ src/parser/Expression.ts
@@ -75,7 +75,7 @@ export class Function extends AstNode implements Expression {
         readonly returns: ValueKind,
         readonly body: Block,
         readonly keyword: Token,
-        readonly end: Token
+        readonly endKeyword: Token
     ) {
         super("Function");
     }
@@ -88,7 +88,7 @@ export class Function extends AstNode implements Expression {
         return {
             file: this.keyword.location.file,
             start: this.keyword.location.start,
-            end: this.end.location.end,
+            end: this.endKeyword.location.end,
         };
     }
 }
